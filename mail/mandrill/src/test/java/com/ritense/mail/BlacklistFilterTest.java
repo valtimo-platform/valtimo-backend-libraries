0820790dd49a898f77baa1d9491c6d47bc17c990
Hildebrand-Ritense <54576297+Hildebrand-Ritense@users.noreply.github.com>
2021-11-19 09:05:57 +0100

    Bugfix for test assertion static imports
    fixed the usage of the deprecated junit 4 assertions in favor of the hamcrest matcher, modified the jvmTarget option setting so that the test Kotlin compilation is done with target jvm 13 as well (#13)

--- mail/mandrill/src/test/java/com/ritense/mail/BlacklistFilterTest.java
+++ mail/mandrill/src/test/java/com/ritense/mail/BlacklistFilterTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
--- mail/mandrill/src/test/java/com/ritense/mail/WhitelistFilterTest.java
+++ mail/mandrill/src/test/java/com/ritense/mail/WhitelistFilterTest.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
