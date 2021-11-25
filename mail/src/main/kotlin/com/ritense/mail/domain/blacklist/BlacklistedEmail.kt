6cc01e1eb19c444b39494eede25baf1cdc90085a
Tom Bokma <tom.bokma@ritense.com>
2021-11-25 17:01:39 +0100

    added contraints

--- mail/src/main/kotlin/com/ritense/mail/domain/blacklist/BlacklistedEmail.kt
+++ mail/src/main/kotlin/com/ritense/mail/domain/blacklist/BlacklistedEmail.kt
@@ -1,17 +1,21 @@
 package com.ritense.mail.domain.blacklist
 
 import com.ritense.valtimo.contract.validation.Validatable
+import org.hibernate.validator.constraints.Length
 import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
+import javax.validation.constraints.NotBlank
 
 @Entity
 @Table(name = "blacklisted_email")
 data class BlacklistedEmail(
     @Id
     @Column(name = "email", columnDefinition = "VARCHAR(500)")
+    @field:Length(max = 500)
+    @field:NotBlank
     val emailAddress: String,
 
     @Column(name = "date_created", columnDefinition = "DATETIME", nullable = false)
