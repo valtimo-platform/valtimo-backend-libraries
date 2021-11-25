278863385f183725533c8efb72034a640d1683e9
Huib van Straten <huib.van.straten@ritense.com>
2021-11-25 09:43:45 +0100

    Added empty FlowmailerDispatcher
    Added empty FlowmailerDispatcher with config, autoConfiguration and necessary tests for checking if right bean is instantiated.

---
+++ mail/flowmailer/src/main/kotlin/com/ritense/mail/flowmailer/autoconfigure/FlowmailerAutoConfiguration.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015-2020 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.mail.flowmailer.autoconfigure
+
+import com.ritense.mail.MailDispatcher
+import com.ritense.mail.flowmailer.config.FlowmailerProperties
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import com.ritense.mail.flowmailer.service.FlowmailerMailDispatcher
+
+@Configuration
+@EnableConfigurationProperties(value = [FlowmailerProperties::class])
+class FlowmailerAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(MailDispatcher::class)
+    fun mailDispatcher(
+        flowmailerProperties: FlowmailerProperties
+    ): MailDispatcher {
+        return FlowmailerMailDispatcher(flowmailerProperties)
+    }
+}
