6dacace5ee84441d0a1784d6007be26cd24cb5b9
Tom Bokma <tom.bokma@ritense.com>
2021-12-14 12:10:05 +0100

    clean up

--- mail/flowmailer/src/test/kotlin/com/ritense/mail/flowmailer/connector/FlowmailerConnectorPropertiesIntTest.kt
+++ mail/flowmailer/src/test/kotlin/com/ritense/mail/flowmailer/connector/FlowmailerConnectorPropertiesIntTest.kt
@@ -1,17 +1,17 @@
 package com.ritense.mail.flowmailer.connector
 
 import com.ritense.mail.flowmailer.BaseIntegrationTest
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import javax.inject.Inject
 
-internal class FlowmailerConnectorPropertiesIntTest: BaseIntegrationTest() {
+internal class FlowmailerConnectorPropertiesIntTest : BaseIntegrationTest() {
 
     @Inject
     lateinit var flowmailerConnectorProperties: FlowmailerConnectorProperties
 
     @Test
     fun `should instantiate FlowmailerConnector bean`() {
-        Assertions.assertThat(flowmailerConnectorProperties).isNotNull
+        assertThat(flowmailerConnectorProperties).isNotNull
     }
 }
