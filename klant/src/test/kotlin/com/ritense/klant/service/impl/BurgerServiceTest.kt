5b8b6ac078d9e1703b3679c8e7dbb8a3a16374bb
Tom Bokma <tom.bokma@ritense.com>
2021-12-14 16:12:47 +0100

    added unit test for burgerService

---
+++ klant/src/test/kotlin/com/ritense/klant/service/impl/BurgerServiceTest.kt
@@ -0,0 +1,21 @@
+package com.ritense.klant.service.impl
+
+import com.ritense.klant.client.OpenKlantClient
+import com.ritense.klant.domain.klanten.Klant
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+internal class BurgerServiceTest {
+
+    var openKlantClient = mock(OpenKlantClient::class.java)
+    var burgerService = BurgerService(openKlantClient)
+
+    @Test
+    fun `burgerService calls openklant client with BSN for burger`() {
+        `when`(openKlantClient.getKlant("123")).thenReturn(mock(Klant::class.java))
+        burgerService.getBurger("123")
+        verify(openKlantClient).getKlant("123")
+    }
+}
