70939a3ab9b3a33ac9ca69bf2b4731d002819b67
Marijn Verbeek <marijn.verbeek@ritense.com>
2022-01-04 15:56:23 +0100

    moved LiquibaseRunnerExtension to a separate module as test only classes should only be exposed in test

--- objects-api/src/test/kotlin/com/ritense/objectsapi/BaseIntegrationTest.kt
+++ objects-api/src/test/kotlin/com/ritense/objectsapi/BaseIntegrationTest.kt
@@ -17,7 +17,7 @@
 package com.ritense.objectsapi
 
 import com.ritense.valtimo.contract.authentication.UserManagementService
-import com.ritense.valtimo.contract.junit.extension.LiquibaseRunnerExtension
+import com.ritense.testutilscommon.junit.extension.LiquibaseRunnerExtension
 import com.ritense.valtimo.contract.mail.MailSender
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Tag
