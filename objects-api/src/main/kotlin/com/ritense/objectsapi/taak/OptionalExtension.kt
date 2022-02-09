447cab627b4c18ed04ae04fc4d93d90da16d4b30
Ivo Zaal <ivo.zaal@ritense.com>
2022-02-09 16:42:02 +0100

    refactor ProcessDocumentServices together

--- objects-api/src/main/kotlin/com/ritense/objectsapi/taak/OptionalExtension.kt
+++
@@ -1,5 +0,0 @@
-package com.ritense.objectsapi.taak
-
-import java.util.Optional
-
-fun <T> Optional<T>.orNull(): T? = orElse(null)
--- objects-api/src/main/kotlin/com/ritense/objectsapi/taak/TaakObjectAutoConfiguration.kt
+++ objects-api/src/main/kotlin/com/ritense/objectsapi/taak/TaakObjectAutoConfiguration.kt
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2015-2022 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ritense.objectsapi.taak
 
-import com.ritense.document.service.DocumentService
 import com.ritense.objectsapi.taak.initiator.BsnProvider
 import com.ritense.objectsapi.taak.initiator.DocumentInitiatorProvider
 import com.ritense.objectsapi.taak.initiator.KvkProvider
@@ -10,8 +25,7 @@ import com.ritense.objectsapi.taak.resolve.PlaceHolderValueResolverService
 import com.ritense.objectsapi.taak.resolve.ProcessVariableValueResolver
 import com.ritense.openzaak.service.ZaakInstanceLinkService
 import com.ritense.openzaak.service.ZaakRolService
-import com.ritense.processdocument.service.ProcessDocumentAssociationService
-import com.ritense.valtimo.service.CamundaProcessService
+import com.ritense.processdocument.service.ProcessDocumentService
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
@@ -76,12 +90,4 @@ class TaakObjectAutoConfiguration {
         return DocumentInitiatorProvider(processDocumentService, zaakInstanceLinkService, zaakRolService)
     }
 
-    @Bean
-    fun taakProcessDocumentService(
-        processDocumentAssociationService: ProcessDocumentAssociationService,
-        documentService: DocumentService,
-        camundaProcessService: CamundaProcessService
-    ): ProcessDocumentService {
-        return ProcessDocumentService(processDocumentAssociationService, documentService, camundaProcessService)
-    }
 }
