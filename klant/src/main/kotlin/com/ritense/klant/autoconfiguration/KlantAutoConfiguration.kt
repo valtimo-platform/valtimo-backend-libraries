a64eddbbb317ee84c3508dd01c4a2b5468696e58
Ivo Zaal <ivo.zaal@ritense.com>
2022-02-21 18:05:00 +0100

    Made klant-api optional

--- klant/src/main/kotlin/com/ritense/klant/autoconfiguration/KlantAutoConfiguration.kt
+++ klant/src/main/kotlin/com/ritense/klant/autoconfiguration/KlantAutoConfiguration.kt
@@ -24,11 +24,13 @@ import com.ritense.klant.service.impl.BurgerService
 import com.ritense.openzaak.service.ZaakInstanceLinkService
 import com.ritense.openzaak.service.ZaakRolService
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ConditionalOnProperty("valtimo.openklant.url")
 @EnableConfigurationProperties(OpenKlantClientProperties::class)
 class KlantAutoConfiguration {
 
