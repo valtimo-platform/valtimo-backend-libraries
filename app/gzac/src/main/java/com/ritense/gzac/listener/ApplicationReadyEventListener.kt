ced054d8bfce66ba1014cc1f1db4c710bb889523
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2022-02-28 16:45:24 +0100

    Configure default document roles + default form links

--- app/gzac/src/main/java/com/ritense/gzac/listener/ApplicationReadyEventListener.kt
+++ app/gzac/src/main/java/com/ritense/gzac/listener/ApplicationReadyEventListener.kt
@@ -4,6 +4,7 @@ import com.ritense.connector.domain.ConnectorType
 import com.ritense.connector.service.ConnectorService
 import com.ritense.contactmoment.connector.ContactMomentProperties
 import com.ritense.document.domain.event.DocumentDefinitionDeployedEvent
+import com.ritense.document.service.DocumentDefinitionService
 import com.ritense.objectsapi.opennotificaties.OpenNotificatieProperties
 import com.ritense.objectsapi.productaanvraag.ProductAanvraagProperties
 import com.ritense.objectsapi.productaanvraag.ProductAanvraagTypeMapping
@@ -36,7 +37,8 @@ class ApplicationReadyEventListener(
     private val objectSyncService: ObjectSyncService,
     private val processDocumentAssociationService: ProcessDocumentAssociationService,
     private val zaakTypeLinkService: ZaakTypeLinkService,
-    private val informatieObjectTypeLinkService: InformatieObjectTypeLinkService
+    private val informatieObjectTypeLinkService: InformatieObjectTypeLinkService,
+    private val documentDefinitionService: DocumentDefinitionService,
 ) {
 
     @EventListener(ApplicationReadyEvent::class)
@@ -48,6 +50,7 @@ class ApplicationReadyEventListener(
     fun handleDocumentDefinitionDeployed(event: DocumentDefinitionDeployedEvent) {
         linkProcess(event)
         connectZaakType(event)
+        setDocumentDefinitionRole(event)
     }
 
     fun createConnectors() {
@@ -260,6 +263,13 @@ class ApplicationReadyEventListener(
                 )
             )
         }
+    }
+
+    fun setDocumentDefinitionRole(event: DocumentDefinitionDeployedEvent) {
+        documentDefinitionService.putDocumentDefinitionRoles(
+            event.documentDefinition().id().name(),
+            setOf("ROLE_ADMIN", "ROLE_USER")
+        )
     }
 
     companion object {
