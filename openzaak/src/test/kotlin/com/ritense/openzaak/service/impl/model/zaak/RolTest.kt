26c935a2a69ab7866d6254ae6e938465fdd7386e
Rick Veenstra <rick.veenstra@ritense.com>
2022-03-23 08:22:30 +0100

    feature: Expanded RolTest with more tests

--- openzaak/src/test/kotlin/com/ritense/openzaak/service/impl/model/zaak/RolTest.kt
+++ openzaak/src/test/kotlin/com/ritense/openzaak/service/impl/model/zaak/RolTest.kt
@@ -1,13 +1,23 @@
 package com.ritense.openzaak.service.impl.model.zaak
 
-import com.ritense.openzaak.service.impl.Mapper
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.ritense.openzaak.service.impl.model.zaak.betrokkene.RolNatuurlijkPersoon
 import com.ritense.openzaak.service.impl.model.zaak.betrokkene.RolNietNatuurlijkPersoon
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.net.URI
 import kotlin.test.assertEquals
 
 internal class RolTest {
+
+    private lateinit var mapper: ObjectMapper
+
+    @BeforeEach
+    fun setUp() {
+        mapper = ObjectMapper().registerModule(KotlinModule())
+    }
 
     @Test
     fun `should serialize natuurlijk persoon`() {
@@ -22,9 +32,15 @@ internal class RolTest {
             )
         )
 
-        val result = Mapper.get().writeValueAsString(rol)
+        val result = mapper.writeValueAsString(rol)
 
-        val expectation =  "{\"zaak\":\"http://zaak.uri\",\"betrokkene\":\"http://betrokkene.uri\",\"betrokkeneType\":\"natuurlijk_persoon\",\"roltype\":\"http://role.type\",\"roltoelichting\":\"role description\",\"betrokkeneIdentificatie\":{\"inpBsn\":\"bsn\"}}"
+        val expectation =  "{" +
+            "\"zaak\":\"http://zaak.uri\"," +
+            "\"betrokkene\":\"http://betrokkene.uri\"," +
+            "\"betrokkeneType\":\"natuurlijk_persoon\"," +
+            "\"roltype\":\"http://role.type\"," +
+            "\"roltoelichting\":\"role description\"," +
+            "\"betrokkeneIdentificatie\":{\"inpBsn\":\"bsn\"}}"
 
         assertEquals(expectation, result)
     }
@@ -42,10 +58,50 @@ internal class RolTest {
             )
         )
 
-        val result = Mapper.get().writeValueAsString(rol)
+        val result = mapper.writeValueAsString(rol)
 
-        val expectation =  "{\"zaak\":\"http://zaak.uri\",\"betrokkene\":\"http://betrokkene.uri\",\"betrokkeneType\":\"niet_natuurlijk_persoon\",\"roltype\":\"http://role.type\",\"roltoelichting\":\"role description\",\"betrokkeneIdentificatie\":{\"innNnpId\":\"kvk\"}}"
+        val expectation =  "{" +
+            "\"zaak\":\"http://zaak.uri\"," +
+            "\"betrokkene\":\"http://betrokkene.uri\"," +
+            "\"betrokkeneType\":\"niet_natuurlijk_persoon\"," +
+            "\"roltype\":\"http://role.type\"," +
+            "\"roltoelichting\":\"role description\"," +
+            "\"betrokkeneIdentificatie\":{\"innNnpId\":\"kvk\"}}"
 
         assertEquals(expectation, result)
+    }
+
+    @Test
+    fun `should deserialize natuurlijk persoon`() {
+        val json =   "{" +
+            "\"zaak\":\"http://zaak.uri\"," +
+            "\"betrokkene\":\"http://betrokkene.uri\"," +
+            "\"betrokkeneType\":\"natuurlijk_persoon\"," +
+            "\"roltype\":\"http://role.type\"," +
+            "\"roltoelichting\":\"role description\"," +
+            "\"betrokkeneIdentificatie\":{\"inpBsn\":\"bsn\"}}"
+
+
+        val result = mapper.readValue(json, Rol::class.java)
+
+        assertTrue(result.betrokkeneIdentificatie is RolNatuurlijkPersoon)
+        assertEquals("bsn", (result.betrokkeneIdentificatie as RolNatuurlijkPersoon).inpBsn)
+    }
+
+    @Test
+    fun `should deserialize niet natuurlijk persoon`() {
+        val json =   "{" +
+            "\"zaak\":\"http://zaak.uri\"," +
+            "\"betrokkene\":\"http://betrokkene.uri\"," +
+            "\"betrokkeneType\":\"niet_natuurlijk_persoon\"," +
+            "\"roltype\":\"http://role.type\"," +
+            "\"roltoelichting\":\"role description\"," +
+            "\"betrokkeneIdentificatie\":{\"innNnpId\":\"kvk\"}}"
+
+
+        val result = mapper.readValue(json, Rol::class.java)
+
+        assertTrue(result.betrokkeneIdentificatie is RolNietNatuurlijkPersoon)
+        assertEquals("kvk", (result.betrokkeneIdentificatie as RolNietNatuurlijkPersoon).innNnpId)
     }
 }
