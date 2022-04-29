7e53e25364ef9f60d43350114c8d540cfb8b9c47
HvS <huib.van.straten@ritense.com>
2022-04-29 13:44:39 +0200

    - Undid the including of taakObjectStatus.gesloten in the function notificationReceived

--- objects-api/src/main/kotlin/com/ritense/objectsapi/taak/TaakObjectListener.kt
+++ objects-api/src/main/kotlin/com/ritense/objectsapi/taak/TaakObjectListener.kt
@@ -67,7 +67,7 @@ class TaakObjectListener(
                 val taakObjectId = event.notification.getObjectId()
                 val taakObjectRecord = connector.getTaakObjectRecord(taakObjectId)
                 val taakObject = taakObjectRecord.record.data
-                if (taakObject.status != TaakObjectStatus.ingediend && taakObject.status != TaakObjectStatus.gesloten) {
+                if (taakObject.status != TaakObjectStatus.ingediend) {
                     return
                 }
                 saveDataAndCompleteTask(taakObject)
