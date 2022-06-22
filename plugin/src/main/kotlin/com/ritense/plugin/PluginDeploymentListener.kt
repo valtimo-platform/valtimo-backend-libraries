de8e9cf57179d1612e356b47f6319033f784d997
Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>
2022-06-22 09:59:46 +0200

    Load plugins on startup (#209)
    * Load plugins on startup
    
    Co-authored-by: Marijn Verbeek <marijn.verbeek@ritense.com>

---
+++ plugin/src/main/kotlin/com/ritense/plugin/PluginDeploymentListener.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015-2022 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.plugin
+
+import com.ritense.plugin.annotation.Plugin
+import com.ritense.plugin.domain.PluginDefinition
+import com.ritense.plugin.repository.PluginDefinitionRepository
+import mu.KotlinLogging
+import org.springframework.boot.context.event.ApplicationStartedEvent
+import org.springframework.context.event.EventListener
+
+class PluginDeploymentListener(
+    private val pluginDefinitionResolver: PluginDefinitionResolver,
+    private val pluginDefinitionRepository: PluginDefinitionRepository
+) {
+
+    @EventListener(ApplicationStartedEvent::class)
+    fun deployPluginDefinitions() {
+        logger.info { "Deploying plugins" }
+        val classes = findPluginClasses()
+
+        classes.forEach { (clazz, pluginAnnotation) ->
+            try {
+                deployPluginDefinition(
+                    PluginDefinition(pluginAnnotation.key, pluginAnnotation.title, pluginAnnotation.description, clazz.name)
+                )
+            } catch (e: Exception) {
+                throw PluginDefinitionNotDeployedException(pluginAnnotation.key, clazz.name, e)
+            }
+        }
+    }
+
+    private fun findPluginClasses() : Map<Class<*>, Plugin> {
+        return pluginDefinitionResolver.findPluginClasses()
+    }
+
+    private fun deployPluginDefinition(pluginDefinition: PluginDefinition): PluginDefinition {
+        logger.info { "Deploying plugin $pluginDefinition.key" }
+        return pluginDefinitionRepository.save(pluginDefinition)
+    }
+
+    companion object {
+        val logger = KotlinLogging.logger {}
+    }
+}
