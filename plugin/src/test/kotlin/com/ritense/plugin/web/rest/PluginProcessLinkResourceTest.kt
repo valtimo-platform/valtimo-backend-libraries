fc88a9107bb99f65785982c27c05a3de251d4424
Ivo Zaal <ivo.zaal@ritense.com>
2022-07-11 10:04:11 +0200

    Added tests

--- plugin/src/test/kotlin/com/ritense/plugin/web/rest/PluginProcessLinkResourceTest.kt
+++ plugin/src/test/kotlin/com/ritense/plugin/web/rest/PluginProcessLinkResourceTest.kt
@@ -2,8 +2,12 @@ package com.ritense.plugin.web.rest
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.ritense.plugin.domain.PluginProcessLink
+import com.ritense.plugin.domain.PluginProcessLinkId
 import com.ritense.plugin.service.PluginService
 import com.ritense.plugin.web.rest.dto.PluginProcessLinkDto
 import com.ritense.valtimo.contract.json.Mapper
@@ -11,8 +15,11 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import java.nio.charset.StandardCharsets
@@ -37,7 +44,55 @@ internal class PluginProcessLinkResourceTest {
 
     @Test
     fun `should list plugin process links`() {
-        TODO()
+        val properties: JsonNode = ObjectMapper().readTree("{\"name\": \"whatever\" }")
+        val id1 = UUID.randomUUID()
+        val id2 = UUID.randomUUID()
+        val processDefinitionId = "pid"
+        val activityId = "aid"
+
+        val processLinks = listOf(
+            PluginProcessLink(
+                PluginProcessLinkId(id1),
+                processDefinitionId,
+                activityId,
+                properties,
+                "pluginConfigurationKey1",
+                "pluginActionDefinitionKey1"
+            ),
+            PluginProcessLink(
+                PluginProcessLinkId(id2),
+                processDefinitionId,
+                activityId,
+                properties,
+                "pluginConfigurationKey2",
+                "pluginActionDefinitionKey2"
+            )
+        )
+
+        whenever(pluginService.getProcessLinks(any(), any())).thenReturn(processLinks)
+
+        mockMvc.perform(
+            get("/api/process-link?processDefinitionId=$processDefinitionId&activityId=$activityId")
+                .characterEncoding(StandardCharsets.UTF_8.name())
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.[0].id").value(id1.toString()))
+            .andExpect(jsonPath("$.[0].processDefinitionId").value(processDefinitionId))
+            .andExpect(jsonPath("$.[0].activityId").value(activityId))
+            .andExpect(jsonPath("$.[0].actionProperties.name").value("whatever"))
+            .andExpect(jsonPath("$.[0].pluginConfigurationKey").value("pluginConfigurationKey1"))
+            .andExpect(jsonPath("$.[0].pluginActionDefinitionKey").value("pluginActionDefinitionKey1"))
+            .andExpect(jsonPath("$.[1].id").value(id2.toString()))
+            .andExpect(jsonPath("$.[1].processDefinitionId").value(processDefinitionId))
+            .andExpect(jsonPath("$.[1].activityId").value(activityId))
+            .andExpect(jsonPath("$.[1].actionProperties.name").value("whatever"))
+            .andExpect(jsonPath("$.[1].pluginConfigurationKey").value("pluginConfigurationKey2"))
+            .andExpect(jsonPath("$.[1].pluginActionDefinitionKey").value("pluginActionDefinitionKey2"))
+
+
+        verify(pluginService).getProcessLinks(processDefinitionId, activityId)
     }
 
     @Test
@@ -67,6 +122,27 @@ internal class PluginProcessLinkResourceTest {
 
     @Test
     fun `should update plugin process link`() {
-        TODO()
+        val properties: JsonNode = ObjectMapper().readTree("{\"name\": \"whatever\" }")
+
+        val pluginProcessLinkDto = PluginProcessLinkDto(
+            id = UUID.randomUUID(),
+            processDefinitionId = UUID.randomUUID().toString(),
+            pluginConfigurationKey = "some-plugin-configuration",
+            activityId = "someActivity",
+            pluginActionDefinitionKey = "some-plugin-action",
+            actionProperties = properties
+        )
+
+        mockMvc.perform(
+            put("/api/process-link/plugin")
+                .characterEncoding(StandardCharsets.UTF_8.name())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(Mapper.INSTANCE.get().writeValueAsString(pluginProcessLinkDto))
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+        )
+            .andDo(print())
+            .andExpect(status().isNoContent)
+
+        verify(pluginService).updateProcessLink(pluginProcessLinkDto)
     }
 }
