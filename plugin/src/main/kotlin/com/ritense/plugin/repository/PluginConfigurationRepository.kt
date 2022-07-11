cb75088ae7c37778d135bb8753ba63c1c5a4f118
Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>
2022-07-11 15:58:18 +0200

    Fixed plugin tests (#238)

--- plugin/src/main/kotlin/com/ritense/plugin/repository/PluginConfigurationRepository.kt
+++ plugin/src/main/kotlin/com/ritense/plugin/repository/PluginConfigurationRepository.kt
@@ -17,6 +17,7 @@
 package com.ritense.plugin.repository
 
 import com.ritense.plugin.domain.PluginConfiguration
+import com.ritense.plugin.domain.PluginConfigurationId
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PluginConfigurationRepository: JpaRepository<PluginConfiguration, String>
+interface PluginConfigurationRepository: JpaRepository<PluginConfiguration, PluginConfigurationId>
