e2710538695bb3d72784a45d54fa49baa4edb530
Ivo Zaal <ivo.zaal@ritense.com>
2022-07-12 15:49:09 +0200

    Added security configuration

--- plugin/src/main/kotlin/com/ritense/plugin/security/config/PluginHttpSecurityConfigurer.kt
+++ plugin/src/main/kotlin/com/ritense/plugin/security/config/PluginHttpSecurityConfigurer.kt
@@ -19,8 +19,10 @@ package com.ritense.plugin.security.config
 import com.ritense.valtimo.contract.authentication.AuthoritiesConstants.ADMIN
 import com.ritense.valtimo.contract.security.config.HttpConfigurerConfigurationException
 import com.ritense.valtimo.contract.security.config.HttpSecurityConfigurer
+import org.springframework.http.HttpMethod.DELETE
 import org.springframework.http.HttpMethod.GET
 import org.springframework.http.HttpMethod.POST
+import org.springframework.http.HttpMethod.PUT
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 
 class PluginHttpSecurityConfigurer: HttpSecurityConfigurer {
@@ -31,6 +33,8 @@ class PluginHttpSecurityConfigurer: HttpSecurityConfigurer {
                 .antMatchers(GET, "/api/plugin/definition").hasAuthority(ADMIN)
                 .antMatchers(GET, "/api/plugin/configuration").hasAuthority(ADMIN)
                 .antMatchers(POST, "/api/plugin/configuration").hasAuthority(ADMIN)
+                .antMatchers(PUT, "/api/plugin/configuration/{pluginConfigurationId}").hasAuthority(ADMIN)
+                .antMatchers(DELETE, "/api/plugin/configuration/{pluginConfigurationId}").hasAuthority(ADMIN)
                 .antMatchers(GET, "/api/plugin/definition/{pluginDefinitionKey}/action").hasAuthority(ADMIN)
         } catch(e: Exception) {
             throw HttpConfigurerConfigurationException(e)
