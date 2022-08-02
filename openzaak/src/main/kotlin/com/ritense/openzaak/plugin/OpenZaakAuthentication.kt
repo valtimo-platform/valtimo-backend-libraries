40c53a58cb7162ff70ea9bc6536c91ede7b42a8a
Theo Beers <theo.beers@ritense.com>
2022-08-02 17:38:27 +0200

    Added authentication plugin for OpenZaak TODO: add tests

---
+++ openzaak/src/main/kotlin/com/ritense/openzaak/plugin/OpenZaakAuthentication.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015-2022 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.openzaak.plugin
+
+import com.ritense.documentenapi.DocumentenApiAuthentication
+import com.ritense.openzaak.service.impl.OpenZaakTokenGeneratorService
+import com.ritense.plugin.annotation.Plugin
+import com.ritense.plugin.annotation.PluginProperty
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.ExchangeFunction
+import reactor.core.publisher.Mono
+
+@Plugin(
+    key = "openzaak-authentication",
+    title = "OpenZaak Authentication",
+    description = "Authentication plugin for OpenZaak"
+)
+class OpenZaakAuthentication(
+    val tokenGeneratorService: OpenZaakTokenGeneratorService
+): DocumentenApiAuthentication {
+    @PluginProperty(key = "clientId", secret = false, required = true)
+    lateinit var clientId: String
+    @PluginProperty(key = "secretKey", secret = true, required = true)
+    lateinit var secretKey: String
+
+    override fun filter(request: ClientRequest, next: ExchangeFunction): Mono<ClientResponse> {
+        val generatedToken = tokenGeneratorService.generateToken(
+            secretKey,
+            clientId
+        )
+        val filteredRequest = ClientRequest.from(request).headers { headers ->
+            headers.setBearerAuth(generatedToken)
+        }.build()
+        return next.exchange(filteredRequest)
+    }
+}
