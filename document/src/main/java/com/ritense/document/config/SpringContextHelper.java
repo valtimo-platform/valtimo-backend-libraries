ef7f37b9f017d1cb672a4876da85df32a76bf059
Tom Bokma <tom.bokma@ritense.com>
2022-08-09 12:14:18 +0200

    sonarqube fixes

--- document/src/main/java/com/ritense/document/config/SpringContextHelper.java
+++ document/src/main/java/com/ritense/document/config/SpringContextHelper.java
@@ -16,8 +16,6 @@
 
 package com.ritense.document.config;
 
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -26,7 +24,7 @@ public class SpringContextHelper implements ApplicationContextAware {
     private static ApplicationContext context;
 
     @Override
-    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) {
         context = applicationContext;
     }
 
