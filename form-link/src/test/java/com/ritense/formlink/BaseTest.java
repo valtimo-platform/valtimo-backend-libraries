a860555403893c5d2179198474b52714edcf9a50
Tom Bokma <tom.bokma@ritense.com>
2022-08-09 11:52:33 +0200

    fixed test

--- form-link/src/test/java/com/ritense/formlink/BaseTest.java
+++ form-link/src/test/java/com/ritense/formlink/BaseTest.java
@@ -16,7 +16,6 @@
 
 package com.ritense.formlink;
 
-import com.ritense.document.config.SpringContextHelper;
 import com.ritense.document.domain.impl.JsonDocumentContent;
 import com.ritense.document.domain.impl.JsonSchema;
 import com.ritense.document.domain.impl.JsonSchemaDocument;
@@ -24,6 +23,7 @@ import com.ritense.document.domain.impl.JsonSchemaDocumentDefinition;
 import com.ritense.document.domain.impl.JsonSchemaDocumentDefinitionId;
 import com.ritense.document.service.DocumentSequenceGeneratorService;
 import com.ritense.form.domain.FormIoFormDefinition;
+import com.ritense.form.domain.SpringContextHelper;
 import com.ritense.form.domain.request.CreateFormDefinitionRequest;
 import com.ritense.formlink.domain.impl.formassociation.CamundaProcessFormAssociation;
 import com.ritense.formlink.domain.impl.formassociation.CamundaProcessFormAssociationId;
