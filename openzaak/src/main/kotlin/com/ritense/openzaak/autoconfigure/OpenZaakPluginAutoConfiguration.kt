11348870b885663eed5d71096042db27f0887595
Ivo Zaal <ivo.zaal@ritense.com>
2022-08-12 15:13:59 +0200

    Fixed issue with classloading plugin

--- openzaak/src/main/kotlin/com/ritense/openzaak/autoconfigure/OpenZaakPluginAutoConfiguration.kt
+++ openzaak/src/main/kotlin/com/ritense/openzaak/autoconfigure/OpenZaakPluginAutoConfiguration.kt
@@ -27,7 +27,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnClass(PluginService::class)
+@ConditionalOnClass(PluginService::class, ZaakUrlProvider::class)
 class OpenZaakPluginAutoConfiguration {
 
     @Bean
