f601e5db34f94be65ef63cd874f83330e1c26439
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2022-09-27 09:10:55 +0200

    #37224 Implement new endpoint for uploading form files to local file storage
     Conflicts:
    	zgw/zaken-api/src/main/kotlin/com/ritense/zakenapi/uploadprocess/ResourceUploadedEventListener.kt
    	zgw/zaken-api/src/main/kotlin/com/ritense/zakenapi/uploadprocess/UploadProcessAutoConfiguration.kt
    	zgw/zaken-api/src/test/kotlin/com/ritense/zakenapi/uploadprocess/ResourceUploadedEventListenerIT.kt

--- zgw/zaken-api/src/main/kotlin/com/ritense/zakenapi/uploadprocess/ResourceUploadedEventListener.kt
+++ zgw/zaken-api/src/main/kotlin/com/ritense/zakenapi/uploadprocess/ResourceUploadedEventListener.kt
@@ -16,60 +16,111 @@
 
 package com.ritense.zakenapi.uploadprocess
 
-import com.ritense.document.domain.impl.JsonSchemaDocumentId
+import com.ritense.document.domain.impl.JsonSchemaDocumentId.existingId
 import com.ritense.document.service.DocumentService
+import com.ritense.processdocument.domain.impl.CamundaProcessInstanceId
 import com.ritense.processdocument.domain.impl.request.StartProcessForDocumentRequest
 import com.ritense.processdocument.service.DocumentDefinitionProcessLinkService
 import com.ritense.processdocument.service.ProcessDocumentService
 import com.ritense.resource.domain.MetadataType
 import com.ritense.resource.domain.TemporaryResourceUploadedEvent
 import com.ritense.resource.service.TemporaryResourceStorageService
+import com.ritense.valtimo.service.CamundaTaskService
 import mu.KotlinLogging
+import org.camunda.bpm.engine.ActivityTypes
+import org.camunda.bpm.engine.RuntimeService
+import org.camunda.bpm.engine.delegate.DelegateTask
+import org.camunda.bpm.engine.delegate.TaskListener
+import org.camunda.bpm.extension.reactor.bus.CamundaSelector
+import org.camunda.bpm.extension.reactor.spring.listener.ReactorTaskListener
 import org.springframework.context.event.EventListener
 import java.util.UUID
 
-class ResourceUploadedEventListener(
+@CamundaSelector(type = ActivityTypes.TASK_USER_TASK, event = TaskListener.EVENTNAME_COMPLETE)
+open class ResourceUploadedEventListener(
     private val resourceService: TemporaryResourceStorageService,
     private val documentService: DocumentService,
     private val processDocumentService: ProcessDocumentService,
-    private val documentDefinitionProcessLinkService: DocumentDefinitionProcessLinkService
-) {
+    private val documentDefinitionProcessLinkService: DocumentDefinitionProcessLinkService,
+    private val runtimeService: RuntimeService,
+    private val camundaTaskService: CamundaTaskService,
+) : ReactorTaskListener() {
 
+    @Synchronized
     @EventListener(TemporaryResourceUploadedEvent::class)
     fun handle(event: TemporaryResourceUploadedEvent) {
         logger.debug { "Handling TemporaryResourceUploadedEvent with resourceId: ${event.resourceId}" }
 
         val metadata = resourceService.getResourceMetadata(event.resourceId)
-        val documentId = metadata[MetadataType.DOCUMENT_ID.key] as String?
+        val caseId = metadata[MetadataType.DOCUMENT_ID.key] as String?
+        val taskId = metadata[MetadataType.TASK_ID.key] as String?
 
-        if (documentId != null) {
-            val documentDefinitionName = documentService.get(documentId).definitionId().name()
-            val link = documentDefinitionProcessLinkService.getDocumentDefinitionProcessLink(documentDefinitionName)
+        if (taskId != null) {
+            addResourceIdToProcessVariables(taskId, event.resourceId)
+        } else if (caseId != null) {
+            startUploadResourceProcesses(caseId, listOf(event.resourceId))
+        }
+    }
 
-            if (link.isPresent && DOCUMENT_UPLOAD != link.get().type) {
-                logger.error { "Wrong link-type found. Found ${link.get().type}, expected $DOCUMENT_UPLOAD" }
-            } else if (link.isPresent && DOCUMENT_UPLOAD == link.get().type) {
-                val result = processDocumentService.startProcessForDocument(
-                    StartProcessForDocumentRequest(
-                        JsonSchemaDocumentId.existingId(UUID.fromString(documentId)),
-                        link.get().id.processDefinitionKey,
-                        mapOf(
-                            RESOURCE_ID_PROCESS_VAR to event.resourceId,
-                        ),
-                    )
-                )
-                if (result.resultingDocument().isEmpty) {
-                    var logMessage = "Errors occurred during starting the document-upload process:"
-                    result.errors().forEach { logMessage += "\n - " + it.asString() }
-                    logger.error { logMessage }
-                }
+    override fun notify(delegateTask: DelegateTask) {
+        val resourceIds = delegateTask.getVariable(UNIQUE_RESOURCE_IDS_PROCESS_VAR) as List<String>?
+        if (!resourceIds.isNullOrEmpty()) {
+            val processInstanceId = CamundaProcessInstanceId(delegateTask.processInstanceId)
+            val caseId = processDocumentService.getDocumentId(processInstanceId, delegateTask).id.toString()
+            startUploadResourceProcesses(caseId, resourceIds)
+        }
+    }
+
+    private fun addResourceIdToProcessVariables(taskId: String, resourceId: String) {
+        val executionId = camundaTaskService.findTaskById(taskId).executionId
+        val resourcesIds = runtimeService.getVariable(executionId, UNIQUE_RESOURCE_IDS_PROCESS_VAR) as List<String>?
+        val newResourcesIds = resourcesIds?.toMutableList()?.add(resourceId) ?: mutableListOf(resourceId)
+        runtimeService.setVariable(executionId, UNIQUE_RESOURCE_IDS_PROCESS_VAR, newResourcesIds)
+    }
+
+    private fun startUploadResourceProcesses(caseId: String, resourceIds: List<String>) {
+        if (assertDocumentUploadLink(caseId)) {
+            resourceIds.forEach { resourceId ->
+                startUploadResourceProcess(caseId, resourceId)
             }
         }
     }
 
+    private fun assertDocumentUploadLink(caseId: String): Boolean {
+        val caseDefinitionName = documentService.get(caseId).definitionId().name()
+        val link = documentDefinitionProcessLinkService.getDocumentDefinitionProcessLink(caseDefinitionName)
+
+        return if (link.isPresent && DOCUMENT_UPLOAD == link.get().type) {
+            true
+        } else if (link.isPresent && DOCUMENT_UPLOAD != link.get().type) {
+            logger.error { "Wrong link-type found. Found ${link.get().type}, expected $DOCUMENT_UPLOAD" }
+            false
+        } else {
+            false
+        }
+    }
+
+    private fun startUploadResourceProcess(caseId: String, resourceId: String) {
+        val result = processDocumentService.startProcessForDocument(
+            StartProcessForDocumentRequest(
+                existingId(UUID.fromString(caseId)),
+                UPLOAD_DOCUMENT_PROCESS_DEFINITION_KEY,
+                mapOf(RESOURCE_ID_PROCESS_VAR to resourceId)
+            )
+        )
+        if (result.resultingDocument().isEmpty) {
+            var logMessage = "Errors occurred during starting the document-upload process:"
+            result.errors().forEach { logMessage += "\n - " + it.asString() }
+            logger.error { logMessage }
+        }
+    }
+
     companion object {
-        val logger = KotlinLogging.logger {}
+        private val logger = KotlinLogging.logger {}
+
         const val RESOURCE_ID_PROCESS_VAR = "resourceId"
+        const val UNIQUE_RESOURCE_IDS_PROCESS_VAR = "resourceIds-082baa14-d0b2-4de2-80c4-d2b3565a57b9"
+        const val UPLOAD_DOCUMENT_PROCESS_DEFINITION_KEY = "document-upload"
         const val DOCUMENT_UPLOAD = "DOCUMENT_UPLOAD"
     }
 }
