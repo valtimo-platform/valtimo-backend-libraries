125cc33bb431e3981a994e8db2948dd93c04fcde
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2022-10-18 10:20:30 +0200

    #37411 rename

--- process-document/src/main/kotlin/com/ritense/processdocument/autoconfigure/ProcessDocumentKotlinAutoConfiguration.kt
+++ process-document/src/main/kotlin/com/ritense/processdocument/autoconfigure/ProcessDocumentKotlinAutoConfiguration.kt
@@ -25,7 +25,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class ProcessDocumentAutoConfigurationK {
+class ProcessDocumentKotlinAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(DocumentDelegate::class)
