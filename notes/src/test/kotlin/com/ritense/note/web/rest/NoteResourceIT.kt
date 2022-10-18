1e459a662f3933e43173f830a6ae245988164378
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2022-10-18 16:06:20 +0200

    #37532 fix test

--- notes/src/test/kotlin/com/ritense/note/web/rest/NoteResourceIT.kt
+++ notes/src/test/kotlin/com/ritense/note/web/rest/NoteResourceIT.kt
@@ -131,7 +131,7 @@ internal class NoteResourceIT : BaseIntegrationTest() {
                 .content(jacksonObjectMapper().writeValueAsString(note))
         )
             .andDo(print())
-            .andExpect(status().isBadRequest)
+            .andExpect(status().isForbidden)
     }
 
     @Test
