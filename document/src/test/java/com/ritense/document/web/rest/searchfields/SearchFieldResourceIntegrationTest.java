7ed356d7d3ccca4a862ff227419738d385bc968a
Rick Veenstra <rick.veenstra@ritense.com>
2022-10-25 10:50:38 +0200

    feature 37577 - Fixed checkstyle errors in the copyright text

--- document/src/test/java/com/ritense/document/web/rest/searchfields/SearchFieldResourceIntegrationTest.java
+++ document/src/test/java/com/ritense/document/web/rest/searchfields/SearchFieldResourceIntegrationTest.java
@@ -1,36 +1,31 @@
 /*
- *  Copyright 2015-2022 Ritense BV, the Netherlands.
+ * Copyright 2015-2022 Ritense BV, the Netherlands.
  *
- *  Licensed under EUPL, Version 1.2 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" basis,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.ritense.document.web.rest.searchfields;
 
 import com.ritense.document.BaseIntegrationTest;
-import com.ritense.document.domain.Document;
-import com.ritense.document.domain.impl.JsonDocumentContent;
-import com.ritense.document.domain.impl.JsonSchemaDocument;
 import com.ritense.document.domain.impl.Mapper;
 import com.ritense.document.domain.impl.searchfield.SearchField;
 import com.ritense.document.domain.impl.searchfield.SearchFieldDatatype;
 import com.ritense.document.domain.impl.searchfield.SearchFieldFieldtype;
 import com.ritense.document.domain.impl.searchfield.SearchFieldMatchtype;
-import com.ritense.document.repository.DocumentRepository;
 import com.ritense.document.service.SearchFieldService;
 import com.ritense.document.web.rest.impl.SearchFieldResource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
