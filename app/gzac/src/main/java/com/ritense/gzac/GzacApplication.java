878bc0d411a25cb4b40fce3966b8db5d90f53c75
Niek Mol <niek.mol@ritense.com>
2022-11-03 10:47:36 +0100

    added shedlock to Valtimo

--- app/gzac/src/main/java/com/ritense/gzac/GzacApplication.java
+++ app/gzac/src/main/java/com/ritense/gzac/GzacApplication.java
@@ -16,10 +16,14 @@
 
 package com.ritense.gzac;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.ritense.*")
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class GzacApplication {
     public static void main(String[] args) {
         SpringApplication.run(GzacApplication.class, args);
