d5878e1664c16b3e39ce8bd784676587f9a3cb74
Niek-Ritense <89913354+Niek-Ritense@users.noreply.github.com>
2022-11-04 10:53:06 +0100

    added shedlock to Valtimo (#374)
    * added shedlock to Valtimo
    
    * Pr feedback

---
+++ core/src/main/java/com/ritense/valtimo/autoconfigure/SchedulerAutoConfiguration.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015-2022 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.valtimo.autoconfigure;
+
+import javax.sql.DataSource;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@Configuration
+@ConditionalOnClass(DataSource.class)
+public class SchedulerAutoConfiguration {
+
+    @Order(HIGHEST_PRECEDENCE + 13)
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
+    }
+}
