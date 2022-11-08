c863d56cd7679623ce89d683b0a3548789712f56
WouterGerarts <wouter.gerarts@ritense.com>
2022-11-08 09:57:35 +0100

    Return null when no tenant

--- tenancy/src/main/kotlin/com/ritense/tenancy/TenantResolver.kt
+++ tenancy/src/main/kotlin/com/ritense/tenancy/TenantResolver.kt
@@ -7,20 +7,14 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 object TenantResolver {
 
-    private const val REQUEST_PARAM_KEY = "tenant"
-
     /**
      * Resolve tenant-id from auth, can be overridden with request param
      * @return Tenant id string
      */
     // resolve tenant-id to "no_tenant", unless "?tenant=my-tenant" is added to url which will use the value of "tenant"
-    fun getTenantId(): String {
+    fun getTenantId(): String? {
         // resolve from auth
-        try {
-            return (SecurityContextHolder.getContext().authentication as DelegatingTenantAuthenticationToken).tenantId
-        } catch (t: Throwable) {
-            throw RuntimeException("Failed to retrieve user tenant-id", t)
-        }
+        return (SecurityContextHolder.getContext().authentication as DelegatingTenantAuthenticationToken?)?.tenantId
     }
 
 }
