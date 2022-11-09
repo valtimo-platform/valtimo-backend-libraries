3768088b11caed8c2dd8f292a9139914f17dd307
Tom Bokma <tom.bokma@ritense.com>
2022-11-09 16:45:18 +0100

    clean

--- tenancy/src/main/kotlin/com/ritense/tenancy/TenantResolver.kt
+++ tenancy/src/main/kotlin/com/ritense/tenancy/TenantResolver.kt
@@ -2,16 +2,8 @@ package com.ritense.tenancy;
 
 import com.ritense.tenancy.config.TenantSpringContextHelper
 import com.ritense.tenancy.web.DelegatingTenantAuthenticationToken
-import com.ritense.valtimo.contract.config.ValtimoProperties
 import org.springframework.security.core.context.SecurityContextHolder
 
-/*
- This Class has some issues:
- - Fixme: During the startup phase of a Spring App the context can be null thus getTenantId is not able to
-    getValtimoProperties. Solved now by providing this via arguments and having 1 overload version.
- - Fixme: SecurityContextHolder.getContext().authentication can be null. Like in above issue when the app is still
-    starting.
- */
 object TenantResolver {
 
     const val DEFAULT_TENANT_ID = ""
@@ -21,19 +13,14 @@ object TenantResolver {
      * @return Tenant id as string
      */
     @JvmStatic
-    fun getTenantId(valtimoProperties: ValtimoProperties): String {
-        return if (valtimoProperties.app.enableTenancy != true) {
+    fun getTenantId(): String {
+        return if (TenantSpringContextHelper.getValtimoProperties().app.enableTenancy != true) {
             DEFAULT_TENANT_ID
         } else {
             (SecurityContextHolder
                 .getContext()
                 .authentication as? DelegatingTenantAuthenticationToken)?.tenantId ?: DEFAULT_TENANT_ID
         }
-    }
-
-    @JvmStatic
-    fun getTenantId(): String {
-        return getTenantId(TenantSpringContextHelper.getValtimoProperties())
     }
 
 }
