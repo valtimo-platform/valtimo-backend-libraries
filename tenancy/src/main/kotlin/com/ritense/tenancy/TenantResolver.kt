9978400901620176812f8469cc7926adba8e53be
Tom Bokma <tom.bokma@ritense.com>
2022-11-09 15:33:22 +0100

    fix test

--- tenancy/src/main/kotlin/com/ritense/tenancy/TenantResolver.kt
+++ tenancy/src/main/kotlin/com/ritense/tenancy/TenantResolver.kt
@@ -2,6 +2,7 @@ package com.ritense.tenancy;
 
 import com.ritense.tenancy.config.TenantSpringContextHelper
 import com.ritense.tenancy.web.DelegatingTenantAuthenticationToken
+import com.ritense.valtimo.contract.config.ValtimoProperties
 import org.springframework.security.core.context.SecurityContextHolder
 
 object TenantResolver {
@@ -13,8 +14,8 @@ object TenantResolver {
      * @return Tenant id as string
      */
     @JvmStatic
-    fun getTenantId(): String {
-        return if (TenantSpringContextHelper.getValtimoProperties().app.enableTenancy != true) {
+    fun getTenantId(valtimoProperties: ValtimoProperties = TenantSpringContextHelper.getValtimoProperties()): String {
+        return if (valtimoProperties.app.enableTenancy != true) {
             DEFAULT_TENANT_ID
         } else {
             (SecurityContextHolder.getContext().authentication as DelegatingTenantAuthenticationToken).tenantId
