54fd7246b93400a95273591590cf3e4e31ca446c
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2023-01-09 11:19:14 +0100

    #47152 Verzoek plugin - Gradle module (#464)
    * #47152 Verzoek plugin - Gradle module
    
    * #47152 change
    
    * merge with dev

---
+++ zgw/verzoek/src/test/kotlin/com/ritense/verzoek/VerzoekAutoConfigurationIntTest.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.verzoek
+
+import com.ritense.BaseIntegrationTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class VerzoekAutoConfigurationIntTest : BaseIntegrationTest() {
+
+    @Test
+    fun `should load application context`() {
+        Assertions.assertThat(true).isTrue
+    }
+}
