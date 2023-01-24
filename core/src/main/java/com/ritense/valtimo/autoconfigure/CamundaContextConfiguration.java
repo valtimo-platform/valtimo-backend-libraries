0c7569f61f8f140292ddcd7fd1c25d8c8b5b1f09
Marijn Verbeek <marijn.verbeek@ritense.com>
2023-01-24 14:26:22 +0100

    added process beans annotation

---
+++ core/src/main/java/com/ritense/valtimo/autoconfigure/CamundaContextConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ *  Licensed under EUPL, Version 1.2 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.ritense.valtimo.autoconfigure;
+
+import com.ritense.valtimo.CamundaBeansPlugin;
+import com.ritense.valtimo.contract.annotation.ProcessBean;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "valtimo.camunda", name = "bean-whitelisting", havingValue = "true")
+public class CamundaContextConfiguration {
+
+    @Bean
+    public CamundaBeansPlugin camundaBeansPlugin(@ProcessBean Map<String, Object> processBeans) {
+        return new CamundaBeansPlugin(new HashMap<>(processBeans));
+    }
+
+}
