b66dbcfc9e9d39e4f03e97768f42e79e7f5f17c3
Niek Mol <niek.mol@ritense.com>
2023-01-31 10:14:27 +0100

    WIP

---
+++ zgw/search/src/main/kotlin/com/ritense/search/domain/SearchField.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015-2022 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.search.domain
+
+import javax.persistence.Column
+import javax.persistence.Id
+
+data class SearchField(
+
+    @Id
+    @Column(name = "owner_id")
+    val ownerId: String,
+    @Column(name = "column_key")
+    val key: String,
+    @Column(name = "title")
+    val title: String?,
+    @Column(name = "path")
+    val path: String,
+    @Column(name = "column_order")
+    val order: Int,
+    @Column(name = "data_type")
+    val dataType: DataType,
+    @Column(name = "field_type")
+    val fieldType: FieldType
+)
