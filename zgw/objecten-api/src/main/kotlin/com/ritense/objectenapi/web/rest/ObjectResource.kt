c85a42313ad13b02afa9367d1b0832114fdbfcd6
Niek Mol <niek.mol@ritense.com>
2023-02-02 13:14:09 +0100

    pr feedback

--- zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/web/rest/ObjectResource.kt
+++ zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/web/rest/ObjectResource.kt
@@ -23,13 +23,11 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
 @RequestMapping(value = ["/api"])
 class ObjectResource(
     val zaakObjectService: ZaakObjectService
-    ) {
+) {
 
     @GetMapping(value = ["/v1/object/form"])
     fun getPrefilledObjectFromObjectUrl(
