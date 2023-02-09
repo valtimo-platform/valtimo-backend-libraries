c14605425623c1ad69f78bfa6dcc0cf277cb1931
Theo Beers <theo.beers@ritense.com>
2023-02-09 13:35:00 +0100

    Reverted changes to TaakObjectDto because the TaakObjectConnector should not have been changed.

--- objects-api/src/main/kotlin/com/ritense/objectsapi/taak/TaakObjectDto.kt
+++ objects-api/src/main/kotlin/com/ritense/objectsapi/taak/TaakObjectDto.kt
@@ -22,7 +22,6 @@ import java.util.UUID
 data class TaakObjectDto(
     val bsn: String?,
     val kvk: String?,
-    val identificatie: List<TaakIdentificatie> = listOf(),
     @JsonProperty("verwerker_taak_id") val verwerkerTaakId: UUID,
     @JsonProperty("formulier_id") val formulierId: String?,
     @JsonProperty("formulier_url") val formulierUrl: String?,
@@ -40,8 +39,3 @@ data class TaakObjectDto(
 enum class TaakObjectStatus {
     open, ingediend, verwerkt, gesloten
 }
-
-data class TaakIdentificatie(
-    val type: String,
-    val value: String,
-)
