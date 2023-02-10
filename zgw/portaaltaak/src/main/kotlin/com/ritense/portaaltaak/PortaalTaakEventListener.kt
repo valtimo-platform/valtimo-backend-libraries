c4c866bac2aa098ee0e031ed3b08ec914ad4cb88
luissoares <luis.soares@ritense.com>
2023-02-10 10:54:17 +0000

    something

--- zgw/portaaltaak/src/main/kotlin/com/ritense/portaaltaak/PortaalTaakEventListener.kt
+++ zgw/portaaltaak/src/main/kotlin/com/ritense/portaaltaak/PortaalTaakEventListener.kt
@@ -17,7 +17,9 @@
 package com.ritense.portaaltaak
 
 import com.fasterxml.jackson.core.JsonPointer
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.convertValue
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.ritense.document.domain.impl.request.ModifyDocumentRequest
@@ -27,15 +29,20 @@ import com.ritense.notificatiesapi.exception.NotificatiesNotificationEventExcept
 import com.ritense.objectenapi.ObjectenApiPlugin
 import com.ritense.objectmanagement.domain.ObjectManagement
 import com.ritense.objectmanagement.service.ObjectManagementService
-import com.ritense.plugin.domain.ActivityType
 import com.ritense.plugin.domain.PluginConfigurationId
 import com.ritense.plugin.service.PluginService
+import com.ritense.processdocument.domain.ProcessInstanceId
 import com.ritense.processdocument.domain.impl.CamundaProcessInstanceId
 import com.ritense.processdocument.service.ProcessDocumentService
 import com.ritense.valtimo.service.CamundaProcessService
+import com.ritense.valtimo.service.CamundaTaskService
+import com.ritense.valueresolver.ValueResolverService
 import java.net.MalformedURLException
 import java.net.URI
+import java.util.*
+import org.camunda.bpm.engine.RuntimeService
 import org.camunda.bpm.engine.TaskService
+import org.camunda.bpm.engine.delegate.VariableScope
 import org.camunda.bpm.engine.impl.persistence.entity.TaskEntity
 import org.camunda.bpm.engine.task.Task
 import org.springframework.context.event.EventListener
@@ -47,7 +54,11 @@ class PortaalTaakEventListener(
     private val processDocumentService: ProcessDocumentService,
     private val processService: CamundaProcessService,
     private val taskService: TaskService,
-    private val documentService: DocumentService
+    private val camundaTaskService: CamundaTaskService,
+    private val documentService: DocumentService,
+    private val runtimeService: RuntimeService,
+    private val valueResolverService: ValueResolverService,
+    private val objectMapper: ObjectMapper
 ) {
 
     @EventListener(NotificatiesApiNotificationReceivedEvent::class)
@@ -67,22 +78,17 @@ class PortaalTaakEventListener(
         pluginService.findPluginConfiguration(PortaaltaakPlugin::class.java) { properties: JsonNode
             ->
             properties.get("objectManagementConfigurationId").textValue().equals(objectManagement.id.toString())
-        }?.run {
+        }?.let {
 
             val taakObject: TaakObject =
-                jacksonObjectMapper().convertValue(getPortaalTaakObjectData(objectManagement, event))
+                objectMapper.convertValue(getPortaalTaakObjectData(objectManagement, event))
             when (taakObject.status) {
                 TaakStatus.INGEDIEND -> {
-                    val task = taskService.createTaskQuery().taskId(taakObject.verwerkerTaakId).singleResult() ?: return
-                    if (!pluginService
-                            .processLinkExists(this.id, task.taskDefinitionKey, ActivityType.USER_TASK_CREATE)
-                    ) {
-                        return
-                    }
+                    val task = getTaskById(taakObject.verwerkerTaakId) ?: return
+                    val receiveData = getReceiveDataActionProperty(task, it.id.id) ?: return
 
-
-                    val instance = pluginService.createInstance(this) as PortaaltaakPlugin
-                    saveDataInDocument(taakObject, task.processInstanceId, task)
+                    val instance = pluginService.createInstance(it) as PortaaltaakPlugin
+                    saveDataInDocument(taakObject, task.processInstanceId, task, receiveData)
                     startProcessToUploadDocuments(
                         taakObject,
                         instance.uploadedDocumentsHandlerProcess,
@@ -92,14 +98,41 @@ class PortaalTaakEventListener(
                     )
                 }
 
-                else -> throw NotificatiesNotificationEventException("", HttpStatus.INTERNAL_SERVER_ERROR)
+                else -> null
             }
         }
     }
 
-    internal fun saveDataInDocument(taakObject: TaakObject, processInstanceId: String, task: Task) {
+    private fun getTaskById(taskId: String) = taskService.createTaskQuery().taskId(taskId).singleResult()
+
+    private fun getReceiveDataActionProperty(task: Task, pluginConfigurationId: UUID): List<DataBindingConfig>? {
+        val processLinks = pluginService.getProcessLinks(task.processDefinitionId, task.taskDefinitionKey)
+        val processLink = processLinks.firstOrNull { processLink ->
+            processLink.pluginConfigurationId == pluginConfigurationId
+        }
+
+        val receiveDataJsonNode = processLink?.actionProperties?.get("receiveData") ?: return null
+
+        val typeRef = object : TypeReference<List<DataBindingConfig>>() {}
+        return objectMapper.treeToValue(receiveDataJsonNode, objectMapper.constructType(typeRef))
+    }
+
+    internal fun saveDataInDocument(
+        taakObject: TaakObject,
+        processInstanceId: String,
+        task: Task,
+        receiveData: List<DataBindingConfig>
+    ) {
         val document =
             processDocumentService.getDocument(CamundaProcessInstanceId(processInstanceId), task as TaskEntity)
+        val task = camundaTaskService.findTaskById(taakObject.verwerkerTaakId)
+        if (!taakObject.verzondenData.isNullOrEmpty()) {
+            val processInstanceId = CamundaProcessInstanceId(task.processInstanceId)
+            val variableScope = getVariableScope(task)
+            val taakObjectData = jacksonObjectMapper().valueToTree<JsonNode>(taakObject.verzondenData)
+            val resolvedValues = getResolvedValues(receiveData, taakObjectData)
+            handleTaakObjectData(processInstanceId, variableScope, resolvedValues)
+        }
         documentService.modifyDocument(
             ModifyDocumentRequest(
                 document.id().toString(),
@@ -116,6 +149,39 @@ class PortaalTaakEventListener(
                 )
             }
         }
+    }
+
+    /**
+     * @param receiveData: [ doc:/streetName  to  "/persoonsData/adres/straatnaam" ]
+     * @param data {"persoonsData":{"adres":{"straatnaam":"Funenpark"}}}
+     * @return mapOf(doc:/streetName to "Funenpark")
+     */
+    private fun getResolvedValues(receiveData: List<DataBindingConfig>, data: JsonNode): Map<String, Any> {
+        return receiveData.associateBy({ it.key }, { getValue(data, it.value) })
+    }
+
+    private fun getValue(data: JsonNode, path: String): Any {
+        val valueNode = data.at(JsonPointer.valueOf(path))
+        if (valueNode.isMissingNode) {
+            throw RuntimeException("Failed to find path '$path' in data: \n${data.toPrettyString()}")
+        }
+        return objectMapper.treeToValue(valueNode, Object::class.java)
+    }
+
+    private fun handleTaakObjectData(
+        processInstanceId: ProcessInstanceId,
+        variableScope: VariableScope,
+        resolvedValues: Map<String, Any>
+    ) {
+        if (resolvedValues.isNotEmpty()) {
+            valueResolverService.handleValues(processInstanceId.toString(), variableScope, resolvedValues)
+        }
+    }
+
+    private fun getVariableScope(task: Task): VariableScope {
+        return runtimeService.createProcessInstanceQuery()
+            .processInstanceId(task.processInstanceId)
+            .singleResult() as VariableScope
     }
 
     internal fun getDocumentenUrls(verzondenData: JsonNode): List<String> {
