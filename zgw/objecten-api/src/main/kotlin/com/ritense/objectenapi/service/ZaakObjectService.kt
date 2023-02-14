97954854acc148cdd23ad5d9eef6288fe5df3786
Niek Mol <niek.mol@ritense.com>
2023-02-14 16:35:48 +0100

    changed getPrefilledObjectFromObjectUrl call to use other types then objectUrl

--- zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/service/ZaakObjectService.kt
+++ zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/service/ZaakObjectService.kt
@@ -24,6 +24,7 @@ import com.ritense.objectenapi.client.ObjectRecord
 import com.ritense.objectenapi.client.ObjectRequest
 import com.ritense.objectenapi.client.ObjectWrapper
 import com.ritense.objectenapi.management.ObjectManagementInfoProvider
+import com.ritense.objectenapi.web.rest.result.FormType
 import com.ritense.objecttypenapi.ObjecttypenApiPlugin
 import com.ritense.objecttypenapi.client.Objecttype
 import com.ritense.plugin.domain.PluginConfigurationId
@@ -37,8 +38,8 @@ import java.util.UUID
 
 class ZaakObjectService(
     val zaakUrlProvider: ZaakUrlProvider,
-    val pluginService : PluginService,
-    val formDefinitionService : FormDefinitionService,
+    val pluginService: PluginService,
+    val formDefinitionService: FormDefinitionService,
     val objectManagementInfoProvider: ObjectManagementInfoProvider
 ) {
     fun getZaakObjectTypes(documentId: UUID): List<Objecttype> {
@@ -55,11 +56,11 @@ class ZaakObjectService(
             }
     }
 
-    private fun getObjectByObjectUrl(objectUrl: URI) : ObjectWrapper? {
+    private fun getObjectByObjectUrl(objectUrl: URI): ObjectWrapper? {
         val objectenApiPlugin = pluginService
             .createInstance(ObjectenApiPlugin::class.java) { properties: JsonNode ->
                 objectUrl.toString().startsWith(properties.get("url").textValue())
-            }?: return null
+            } ?: return null
         return objectenApiPlugin.getObject(objectUrl)
     }
 
@@ -67,7 +68,7 @@ class ZaakObjectService(
         val objectTypePluginInstance = pluginService
             .createInstance(ObjecttypenApiPlugin::class.java) { properties: JsonNode ->
                 objectTypeUrl.toString().startsWith(properties.get("url").textValue())
-            }?: return null
+            } ?: return null
 
         return objectTypePluginInstance.getObjecttype(objectTypeUrl)
     }
@@ -114,19 +115,44 @@ class ZaakObjectService(
         }
     }
 
-    fun getZaakObjectForm(objectUrl: URI): FormDefinition? {
-        logger.debug { "Getting object for url $objectUrl" }
-        val theObject = getObjectByObjectUrl(objectUrl)
+    fun getZaakObjectForm(
+        objectUrl: URI? = null,
+        objectManagementId: UUID? = null,
+        objectId: UUID? = null,
+        formType: FormType? = null
+    ): FormDefinition? {
+        val theObject = if (objectUrl == null) {
+            getObjectByManagementIdAndObjectId(objectManagementId!!, objectId!!)
+        } else {
+            logger.debug { "Getting object for url $objectUrl" }
+            getObjectByObjectUrl(objectUrl)
+        }
         return theObject?.let {
             logger.trace { "Getting objecttype for object $theObject" }
             getObjectTypeByUrl(it.type)
         }?.let {
-            val formName = "${it.name}$FORM_SUFFIX"
+            val formName = if (formType == null) {
+                "${it.name}$FORM_SUFFIX"
+            } else if (formType == FormType.EDITFORM) {
+                objectManagementInfoProvider.getObjectManagementInfo(objectManagementId!!).formDefinitionEdit
+            } else {
+                objectManagementInfoProvider.getObjectManagementInfo(objectManagementId!!).formDefinitionView
+            }
             logger.trace { "Getting form for objecttype $it with formName $formName" }
             formDefinitionService.getFormDefinitionByNameIgnoringCase(formName)
         }
-        ?.orElse(null)
-        ?.preFill(theObject.record.data)
+            ?.orElse(null)
+            ?.preFill(theObject.record.data)
+    }
+
+    private fun getObjectByManagementIdAndObjectId(objectManagementId: UUID, objectId: UUID): ObjectWrapper? {
+        val objectManagement =
+            objectManagementInfoProvider.getObjectManagementInfo(objectManagementId)
+        val objectsApiPlugin =
+            pluginService.createInstance(PluginConfigurationId(objectManagement.objectenApiPluginConfigurationId)) as ObjectenApiPlugin
+        val objectUrl = "${objectsApiPlugin.url}/objects/$objectId"
+        logger.debug { "Getting object for url $objectUrl" }
+        return getObjectByObjectUrl(URI.create(objectUrl))
     }
 
     private fun findZakenApiPlugin(zaakUrl: URI): ZakenApiPlugin {
@@ -140,10 +166,11 @@ class ZaakObjectService(
         return zakenApiPluginInstance
     }
 
-    fun createObject(objectManagementId: UUID, data: JsonNode) : URI {
+    fun createObject(objectManagementId: UUID, data: JsonNode): URI {
         val objectManagementInfo = objectManagementInfoProvider.getObjectManagementInfo(objectManagementId)
 
-        val objecttypeApiPlugin = pluginService.createInstance(PluginConfigurationId(objectManagementInfo.objecttypenApiPluginConfigurationId)) as ObjecttypenApiPlugin
+        val objecttypeApiPlugin =
+            pluginService.createInstance(PluginConfigurationId(objectManagementInfo.objecttypenApiPluginConfigurationId)) as ObjecttypenApiPlugin
         val objectTypeUrl = objecttypeApiPlugin.getObjectTypeUrlById(objectManagementInfo.objecttypeId)
 
         val createObjectRequest = ObjectRequest(
@@ -155,7 +182,8 @@ class ZaakObjectService(
             )
         )
 
-        val objectenApiPlugin = pluginService.createInstance(PluginConfigurationId(objectManagementInfo.objectenApiPluginConfigurationId)) as ObjectenApiPlugin
+        val objectenApiPlugin =
+            pluginService.createInstance(PluginConfigurationId(objectManagementInfo.objectenApiPluginConfigurationId)) as ObjectenApiPlugin
         return objectenApiPlugin.createObject(createObjectRequest).url
     }
 
