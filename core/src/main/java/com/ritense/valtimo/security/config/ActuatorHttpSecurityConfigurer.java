2a27a288fabe682cceb5c4a8dae83a83d0472bee
Tom Bokma <tom.bokma@ritense.com>
2023-03-13 09:23:14 +0100

    spring boot admin change loglevel support

--- core/src/main/java/com/ritense/valtimo/security/config/ActuatorHttpSecurityConfigurer.java
+++ core/src/main/java/com/ritense/valtimo/security/config/ActuatorHttpSecurityConfigurer.java
@@ -57,8 +57,8 @@ public class ActuatorHttpSecurityConfigurer implements HttpSecurityConfigurer, A
                 .antMatchers(GET, "/management/health").hasAuthority(ACTUATOR)
                 .antMatchers(GET, "/management/mappings").hasAuthority(ACTUATOR)
                 .antMatchers(GET, "/management/logfile").hasAuthority(ACTUATOR)
-                .antMatchers(GET, "/management/loggers/**").hasAuthority(ACTUATOR)
-                .antMatchers(POST, "/management/loggers").hasAuthority(ACTUATOR)
+                .antMatchers(GET, "/management/loggers").hasAuthority(ACTUATOR)
+                .antMatchers(POST, "/management/loggers/**").hasAuthority(ACTUATOR)
                 .antMatchers(GET, "/management/info").hasAnyAuthority(ACTUATOR)
                 .and()
                 .httpBasic().authenticationEntryPoint(basicAuthenticationEntryPoint());
