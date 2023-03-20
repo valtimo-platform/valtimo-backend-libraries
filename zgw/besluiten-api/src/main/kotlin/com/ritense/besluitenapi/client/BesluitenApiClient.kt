91cbc1cdfa111fbd2fb14e6ea2e3415c8d5e581c
luissoares <luis.soares@ritense.com>
2023-03-20 09:56:53 +0000

    code review

--- zgw/besluiten-api/src/main/kotlin/com/ritense/besluitenapi/client/BesluitenApiClient.kt
+++ zgw/besluiten-api/src/main/kotlin/com/ritense/besluitenapi/client/BesluitenApiClient.kt
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ritense.besluitenapi.client
 
 import com.ritense.besluitenapi.BesluitenApiAuthentication
 import com.ritense.besluitenapi.domain.BesluitInformatieObject
 import com.ritense.besluitenapi.domain.CreateBesluitInformatieObject
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.toEntity
 import java.net.URI
@@ -15,16 +31,16 @@ class BesluitenApiClient(
         authentication: BesluitenApiAuthentication,
         url: URI,
         besluitInformatieObject: CreateBesluitInformatieObject
-    ): BesluitInformatieObject? {
+    ): ResponseEntity<BesluitInformatieObject>? {
         return webClientBuilder.clone()
             .filter(authentication)
             .build()
             .post()
-            .uri(url)
+            .uri("$url/besluitinformatieobjecten")
             .contentType(MediaType.APPLICATION_JSON)
             .bodyValue(besluitInformatieObject)
             .retrieve()
             .toEntity<BesluitInformatieObject>()
-            .block()?.body
+            .block()
     }
 }
