c6ae48e55360ae3565e3a43ac3232e6ec30bc2ef
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2023-03-30 09:07:54 +0200

    #58772 Make the process-link endpoints generic

---
+++ process-link/src/test/kotlin/com/ritense/processlink/domain/CustomProcessLink.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.processlink.domain
+
+import com.ritense.processlink.domain.CustomProcessLink.Companion.PROCESS_LINK_TYPE_TEST
+import java.util.UUID
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
+
+@Entity
+@DiscriminatorValue(PROCESS_LINK_TYPE_TEST)
+data class CustomProcessLink(
+    override val id: UUID,
+
+    override val processDefinitionId: String,
+
+    override val activityId: String,
+
+    override val activityType: ActivityTypeWithEventName
+
+) : ProcessLink(
+    id,
+    processDefinitionId,
+    activityId,
+    activityType,
+    PROCESS_LINK_TYPE_TEST,
+) {
+
+    override fun copy(
+        id: UUID,
+        processDefinitionId: String,
+        activityId: String,
+        activityType: ActivityTypeWithEventName,
+        processLinkType: String
+    ): CustomProcessLink {
+        return this.copy(
+            id = id,
+            processDefinitionId = processDefinitionId,
+            activityId = activityId,
+            activityType = activityType,
+        )
+    }
+
+    companion object {
+        const val PROCESS_LINK_TYPE_TEST = "test"
+    }
+}
---
+++ process-link/src/test/kotlin/com/ritense/processlink/domain/CustomProcessLinkUpdateRequestDto.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.processlink.domain
+
+import com.ritense.processlink.domain.CustomProcessLink.Companion.PROCESS_LINK_TYPE_TEST
+import com.ritense.processlink.web.rest.dto.ProcessLinkUpdateRequestDto
+import java.util.UUID
+
+data class CustomProcessLinkUpdateRequestDto(
+    override val id: UUID,
+) : ProcessLinkUpdateRequestDto(
+    id,
+    PROCESS_LINK_TYPE_TEST,
+)
