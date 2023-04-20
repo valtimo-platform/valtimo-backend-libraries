93f9b35ea00f56884afed1857a63e4d75d282f23
Marijn Verbeek <marijn.verbeek@ritense.com>
2023-04-20 13:09:25 +0200

    removed todos

--- authorization/src/main/kotlin/com/ritense/authorization/permission/ContainerPermissionCondition.kt
+++ authorization/src/main/kotlin/com/ritense/authorization/permission/ContainerPermissionCondition.kt
@@ -17,7 +17,6 @@ class ContainerPermissionCondition<TO : Any>(
 ) : PermissionCondition() {
     override val permissionFilterType: PermissionFilterType = PermissionFilterType.CONTAINER
     override fun <FROM: Any> isValid(entity: FROM): Boolean {
-        // TODO: retrieve list of entities matching the resourceType, that are related to the OG entity, and for each:
         val mapper = findMapper(entity::class.java) as AuthorizationEntityMapper<FROM, TO>
         val relatedEntities = mapper.mapTo(entity)
         val spec = findChildSpecification()
@@ -33,7 +32,6 @@ class ContainerPermissionCondition<TO : Any>(
         resourceType: Class<T>,
         queryDialectHelper: QueryDialectHelper
     ): Predicate {
-        // TODO: derive the correct Root, CriteriaQuery and CriteriaBuilder (do a join or something with another entity or multiple)
         val authorizationEntityMapperResult = findMapper(resourceType).mapQueryTo(root, query, criteriaBuilder)
         val spec = findChildSpecification()
 
