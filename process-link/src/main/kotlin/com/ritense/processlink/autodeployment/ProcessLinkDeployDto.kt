a6cdf6d147b08ab1cc2de25e7fca3b016df0ed79
Theo <theo.beers@ritense.com>
2023-04-21 16:58:00 +0200

    made processLinkType required for autodeploy

--- process-link/src/main/kotlin/com/ritense/processlink/autodeployment/ProcessLinkDeployDto.kt
+++ process-link/src/main/kotlin/com/ritense/processlink/autodeployment/ProcessLinkDeployDto.kt
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.ritense.processlink.domain.ActivityTypeWithEventName
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, include = JsonTypeInfo.As.PROPERTY, property = "processLinkType")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "processLinkType")
 @JsonIgnoreProperties("processLinkType", allowSetters = true)
 interface ProcessLinkDeployDto {
     val processDefinitionId: String
