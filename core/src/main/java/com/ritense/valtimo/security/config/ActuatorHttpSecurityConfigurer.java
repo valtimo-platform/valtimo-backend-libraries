c4619f647a4fb9693f33b76b101c0f79bd74ed17
Tom Bokma <tom.bokma@ritense.com>
2023-05-10 10:40:10 +0200

    enable log level manipulation via springbootadmin

--- core/src/main/java/com/ritense/valtimo/security/config/ActuatorHttpSecurityConfigurer.java
+++ core/src/main/java/com/ritense/valtimo/security/config/ActuatorHttpSecurityConfigurer.java
@@ -56,6 +56,7 @@ public class ActuatorHttpSecurityConfigurer implements HttpSecurityConfigurer, A
                 .antMatchers(GET, "/management/mappings").hasAuthority(ACTUATOR)
                 .antMatchers(GET, "/management/logfile").hasAuthority(ACTUATOR)
                 .antMatchers(GET, "/management/loggers").hasAuthority(ACTUATOR)
+                .antMatchers(POST, "/management/loggers/**").hasAuthority(ACTUATOR)
                 .antMatchers(GET, "/management/info").hasAnyAuthority(ACTUATOR)
                 .and()
                 .httpBasic().authenticationEntryPoint(basicAuthenticationEntryPoint());
