6c60ec476e481f7be7ec82111d4fee9921f57ce5
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2023-06-07 16:02:03 +0200

    #67301 dashboard implement domain model (#743)
    * #67301 Dashboard implement domain model
    
    * #67301 Dashboard implement domain model
    
    * #66302 Change id to key
    
    * change widget id to a key
    
    * rename to dashboard_widget_configuration

---
+++ dashboard/src/test/kotlin/com/ritense/dashboard/BaseIntegrationTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.dashboard
+
+import com.ritense.valtimo.contract.authentication.UserManagementService
+import com.ritense.valtimo.contract.mail.MailSender
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@Tag("integration")
+abstract class BaseIntegrationTest {
+
+    @MockBean
+    lateinit var userManagementService: UserManagementService
+
+    @MockBean
+    lateinit var mailSender: MailSender
+}
