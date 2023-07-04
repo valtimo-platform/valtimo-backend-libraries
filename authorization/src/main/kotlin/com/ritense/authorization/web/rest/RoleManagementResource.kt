01d53a8aa1ba43ebe0032b69d739711eb3285c27
marijnritense <94897451+marijnritense@users.noreply.github.com>
2023-07-04 10:11:53 +0200

    Update authorization/src/main/kotlin/com/ritense/authorization/web/rest/RoleManagementResource.kt
    Co-authored-by: Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>

--- authorization/src/main/kotlin/com/ritense/authorization/web/rest/RoleManagementResource.kt
+++ authorization/src/main/kotlin/com/ritense/authorization/web/rest/RoleManagementResource.kt
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.transaction.Transactional
+import org.springframework.transaction.annotation.Transactional
 
 @RestController
 @RequestMapping("/api/management", produces = [ValtimoMediaType.APPLICATION_JSON_UTF8_VALUE])
