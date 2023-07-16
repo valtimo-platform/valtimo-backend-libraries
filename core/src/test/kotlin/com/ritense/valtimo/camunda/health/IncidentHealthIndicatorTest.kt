477fdc66adf55b0a18212df0eb9769ed8062a9c1
Thomas Minke <thomas.minke@ritense.com>
2023-07-16 12:36:55 +0200

    Removed variable declaration.

--- core/src/test/kotlin/com/ritense/valtimo/camunda/health/IncidentHealthIndicatorTest.kt
+++ core/src/test/kotlin/com/ritense/valtimo/camunda/health/IncidentHealthIndicatorTest.kt
@@ -1,16 +1,12 @@
 package com.ritense.valtimo.camunda.health
 
-import com.ritense.valtimo.BaseIntegrationTest
-import org.camunda.bpm.engine.ProcessEngine
 import org.camunda.bpm.engine.RuntimeService
-import org.camunda.bpm.engine.impl.IncidentQueryImpl
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.actuate.health.Health
 
 class IncidentHealthIndicatorTest {
@@ -26,14 +22,12 @@ class IncidentHealthIndicatorTest {
     @Test
     fun `should return up when no incidents exist`(){
         whenever(runtimeService.createIncidentQuery().count()).thenReturn(0L)
-        val health = indicator.health()
-        assertEquals(Health.up().build(), health)
+        assertEquals(Health.up().build(), indicator.health())
     }
 
     @Test
     fun `should return unknown when incidents exist`(){
         whenever(runtimeService.createIncidentQuery().count()).thenReturn(2L)
-        val health = indicator.health()
-        assertEquals(Health.unknown().build(), health)
+        assertEquals(Health.unknown().build(), indicator.health())
     }
 }
