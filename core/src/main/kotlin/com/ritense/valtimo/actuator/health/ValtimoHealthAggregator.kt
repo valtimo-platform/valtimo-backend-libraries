e42a365405f65e07b9a949cb2f1a1b247b7a5f5f
Thomas Minke <thomas.minke@ritense.com>
2023-07-26 14:28:56 +0200

    Added custom health status aggregator.

---
+++ core/src/main/kotlin/com/ritense/valtimo/actuator/health/ValtimoHealthAggregator.kt
@@ -0,0 +1,12 @@
+package com.ritense.valtimo.actuator.health
+
+import org.springframework.boot.actuate.health.Status
+import org.springframework.boot.actuate.health.StatusAggregator
+
+class ValtimoHealthAggregator(): StatusAggregator {
+    override fun getAggregateStatus(statuses: MutableSet<Status>): Status {
+        if(statuses.stream().anyMatch { s -> s.equals(Status.DOWN) }) return Status.DOWN
+        if(statuses.stream().anyMatch { s -> s.equals(Status.UNKNOWN) }) return Status.UNKNOWN
+        return Status.UP
+    }
+}
---
+++ core/src/test/kotlin/com/ritense/valtimo/actuator/health/ValtimoHealthAggregatorTest.kt
@@ -0,0 +1,45 @@
+package com.ritense.valtimo.actuator.health
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.actuate.health.Status
+
+class ValtimoHealthAggregatorTest {
+    lateinit var aggregator: ValtimoHealthAggregator
+
+    @BeforeEach
+    fun beforeEach() {
+        aggregator = ValtimoHealthAggregator()
+    }
+
+    @Test
+    fun `should return up when all components are up`(){
+        val statusSet = HashSet<Status>()
+        statusSet.add(Status.UP)
+        statusSet.add(Status.UP)
+        statusSet.add(Status.UP)
+
+        Assertions.assertEquals(Status.UP, aggregator.getAggregateStatus(statusSet))
+    }
+
+    @Test
+    fun `should return unknown when one of the components is unknown`(){
+        val statusSet = HashSet<Status>()
+        statusSet.add(Status.UP)
+        statusSet.add(Status.UP)
+        statusSet.add(Status.UNKNOWN)
+
+        Assertions.assertEquals(Status.UNKNOWN, aggregator.getAggregateStatus(statusSet))
+    }
+
+    @Test
+    fun `should return down when one of the components is down`(){
+        val statusSet = HashSet<Status>()
+        statusSet.add(Status.UP)
+        statusSet.add(Status.UNKNOWN)
+        statusSet.add(Status.DOWN)
+
+        Assertions.assertEquals(Status.DOWN, aggregator.getAggregateStatus(statusSet))
+    }
+}
