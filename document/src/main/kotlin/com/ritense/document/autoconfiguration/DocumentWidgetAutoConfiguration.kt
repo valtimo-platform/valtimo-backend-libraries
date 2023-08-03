403916dca74e67a1b744bb6027c46e1f43ae8de9
Theo <theo.beers@ritense.com>
2023-08-03 21:37:20 +0200

    renamed DocumentWidgetAutoConfiguration

--- document/src/main/kotlin/com/ritense/document/autoconfiguration/DocumentWidgetAutoConfiguration.kt
+++ document/src/main/kotlin/com/ritense/document/autoconfiguration/DocumentWidgetAutoConfiguration.kt
@@ -24,7 +24,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class DocumentWidgetAutoconfiguration {
+class DocumentWidgetAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(DocumentWidgetDataSource::class)
