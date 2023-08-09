dcf302b9d08fbbbd504252f0204293fba7620159
Tom Bokma <tom.bokma@ritense.com>
2023-08-09 13:11:04 +0200

    fixing tenantId

--- document/src/main/kotlin/com/ritense/document/util/hibernate/HibernateConfig.kt
+++ document/src/main/kotlin/com/ritense/document/util/hibernate/HibernateConfig.kt
@@ -16,7 +16,7 @@
 
 package com.ritense.document.util.hibernate
 
-import org.hibernate.integrator.spi.Integrator
+import org.hibernate.jpa.boot.spi.IntegratorProvider
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer
 
 class HibernateConfig(
@@ -24,18 +24,7 @@ class HibernateConfig(
 ) : HibernatePropertiesCustomizer {
 
     override fun customize(hibernateProperties: MutableMap<String, Any>) {
-        // Assuming you have a mutable map called hibernateProperties and a variable called integrator
-        val integratorProviderKey = "hibernate.integrator_provider"
-
-        // Check if the key already exists in the map
-        if (hibernateProperties.containsKey(integratorProviderKey)) {
-            // If the key exists, retrieve the current list and append the new integrator to it
-            val currentIntegratorList = hibernateProperties[integratorProviderKey] as MutableList<Integrator>
-            hibernateProperties[integratorProviderKey] = currentIntegratorList + integrator
-        } else {
-            // If the key doesn't exist, create a new list containing the integrator and assign it to the key
-            hibernateProperties[integratorProviderKey] = mutableListOf(integrator)
-        }
+        hibernateProperties["hibernate.integrator_provider"] = IntegratorProvider { listOf(integrator) }
     }
 
 }
