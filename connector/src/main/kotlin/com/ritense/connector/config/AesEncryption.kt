4c66106a3e3543b2f86a77f8056c81b190255d0f
marijnritense <94897451+marijnritense@users.noreply.github.com>
2023-08-21 13:43:47 +0200

    changed encryption algorithm (#836)

--- connector/src/main/kotlin/com/ritense/connector/config/AesEncryption.kt
+++ connector/src/main/kotlin/com/ritense/connector/config/AesEncryption.kt
@@ -16,30 +16,65 @@
 
 package com.ritense.connector.config
 
+import java.nio.ByteBuffer
+import java.security.SecureRandom
+import java.security.spec.AlgorithmParameterSpec
 import java.util.Base64
 import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 class AesEncryption(secret: String) {
-    private val key = SecretKeySpec(secret.toByteArray(), AES)
+    private val key = SecretKeySpec(secret.toByteArray(), "AES")
+    private val secureRandom = SecureRandom()
 
     fun encrypt(attribute: String): String {
-        val cipher = createCipher(Cipher.ENCRYPT_MODE)
-        return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.toByteArray()))
+        return PREFIX.plus(String(Base64.getEncoder().encode(encrypt(attribute, key))))
     }
 
     fun decrypt(value: String): String {
-        val cipher = createCipher(Cipher.DECRYPT_MODE)
-        return String(cipher.doFinal(Base64.getDecoder().decode(value)))
+        return if (value.startsWith(PREFIX)) {
+            val valueWithoutPrefix = value.substring(PREFIX.length)
+            decrypt(Base64.getDecoder().decode(valueWithoutPrefix), key)
+        } else {
+            val cipher = createOldDecryptCipher()
+            String(cipher.doFinal(Base64.getDecoder().decode(value)))
+        }
     }
 
-    private fun createCipher(operationMode:Int): Cipher {
+    fun encrypt(plaintext: String, secretKey: SecretKey): ByteArray? {
+        val iv = ByteArray(GCM_IV_LENGTH)
+        secureRandom.nextBytes(iv)
+        val cipher = Cipher.getInstance(AES_GCM_NOPADDING)
+        val parameterSpec = GCMParameterSpec(128, iv)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec)
+
+        val cipherText = cipher.doFinal(plaintext.toByteArray())
+        val byteBuffer: ByteBuffer = ByteBuffer.allocate(iv.size + cipherText.size)
+        byteBuffer.put(iv)
+        byteBuffer.put(cipherText)
+        return byteBuffer.array()
+    }
+
+    fun decrypt(cipherMessage: ByteArray, secretKey: SecretKey): String {
+        val cipher = Cipher.getInstance(AES_GCM_NOPADDING)
+        val gcmIv: AlgorithmParameterSpec = GCMParameterSpec(128, cipherMessage, 0, GCM_IV_LENGTH)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmIv)
+        val plainText = cipher.doFinal(cipherMessage, GCM_IV_LENGTH, cipherMessage.size - GCM_IV_LENGTH)
+        return String(plainText)
+    }
+
+    private fun createOldDecryptCipher(): Cipher {
         val cipher = Cipher.getInstance(AES)
-        cipher.init(operationMode, key)
+        cipher.init(Cipher.DECRYPT_MODE, key)
         return cipher
     }
 
     companion object {
+        const val AES_GCM_NOPADDING = "AES/GCM/NoPadding"
         const val AES = "AES"
+        const val PREFIX = "{AES/GCM/NoPadding}"
+        const val GCM_IV_LENGTH = 12
     }
 }
