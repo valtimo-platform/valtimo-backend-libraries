278330cdd05ed4d569eef686e44381a4642f74f8
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2023-09-12 13:41:08 +0200

    #50055 Rename form flow API endpoints (#872)
    * #50055 Rename form flow API endpoints
    
    * fix
    
    * @Deprecate
    
    * Revert "@Deprecate"
    
    This reverts commit 2b219d8dbe2d3fda36176106109d42954673b6a1.
    
    * fix
    
    ---------
    
    Co-authored-by: Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>

--- form-flow-valtimo/src/main/kotlin/com/ritense/valtimo/formflow/security/ValtimoFormFlowHttpSecurityConfigurer.kt
+++ form-flow-valtimo/src/main/kotlin/com/ritense/valtimo/formflow/security/ValtimoFormFlowHttpSecurityConfigurer.kt
@@ -27,16 +27,16 @@ class ValtimoFormFlowHttpSecurityConfigurer: HttpSecurityConfigurer {
     override fun configure(http: HttpSecurity) {
         try {
             http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/v1/form-flow/{formFlowInstanceId}")
-                .hasAuthority(USER)
-                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/{formFlowId}/step/{stepInstanceId}")
-                .hasAuthority(USER)
-                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/{formFlowId}/back")
-                .hasAuthority(USER)
-                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/{formFlowId}/save")
-                .hasAuthority(USER)
-                .antMatchers(HttpMethod.GET, "/api/v1/process-link/form-flow-definition")
-                .hasAuthority(ADMIN)
+                .antMatchers(HttpMethod.GET, "/api/v1/form-flow/{formFlowInstanceId}").hasAuthority(USER)
+                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/{formFlowId}/step/{stepInstanceId}").hasAuthority(USER)
+                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/{formFlowId}/back").hasAuthority(USER)
+                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/{formFlowId}/save").hasAuthority(USER)
+                .antMatchers(HttpMethod.GET, "/api/v1/process-link/form-flow-definition").hasAuthority(ADMIN)
+                .antMatchers(HttpMethod.GET, "/api/v1/form-flow/instance/{formFlowInstanceId}").hasAuthority(USER)
+                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/instance/{formFlowId}/step/instance/{stepInstanceId}").hasAuthority(USER)
+                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/instance/{formFlowId}/back").hasAuthority(USER)
+                .antMatchers(HttpMethod.POST, "/api/v1/form-flow/instance/{formFlowId}/save").hasAuthority(USER)
+                .antMatchers(HttpMethod.GET, "/api/v1/form-flow/definition").hasAuthority(ADMIN)
         } catch (e: Exception) {
             throw HttpConfigurerConfigurationException(e)
         }
