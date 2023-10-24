06b813f6d9f9b4fc5f006230daf38e9768cffe99
Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>
2023-10-24 16:04:21 +0200

    Custom angular form flow steps (#982)

--- form-flow-valtimo/src/main/kotlin/com/ritense/valtimo/formflow/autoconfigure/FormFlowValtimoAutoConfiguration.kt
+++ form-flow-valtimo/src/main/kotlin/com/ritense/valtimo/formflow/autoconfigure/FormFlowValtimoAutoConfiguration.kt
@@ -33,6 +33,7 @@ import com.ritense.valtimo.formflow.FormFlowProcessLinkActivityHandler
 import com.ritense.valtimo.formflow.FormFlowTaskOpenResultProperties
 import com.ritense.valtimo.formflow.FormLinkNewProcessFormFlowProviderImpl
 import com.ritense.valtimo.formflow.common.ValtimoFormFlow
+import com.ritense.valtimo.formflow.handler.FormFlowStepTypeCustomComponentHandler
 import com.ritense.valtimo.formflow.service.FormFlowSupportedProcessLinksHandler
 import com.ritense.valtimo.formflow.handler.FormFlowStepTypeFormHandler
 import com.ritense.valtimo.formflow.mapper.FormFlowProcessLinkMapper
@@ -138,6 +139,12 @@ class FormFlowValtimoAutoConfiguration {
             documentService,
             objectMapper
         )
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(FormFlowStepTypeCustomComponentHandler::class)
+    fun formFlowStepTypeCustomComponentHandler(): FormFlowStepTypeCustomComponentHandler {
+        return FormFlowStepTypeCustomComponentHandler()
     }
 
     @Bean
---
+++ form-flow-valtimo/src/test/kotlin/com/ritense/valtimo/formflow/handler/FormFlowStepTypeCustomComponentHandlerIT.kt
@@ -0,0 +1,28 @@
+package com.ritense.valtimo.formflow.handler
+
+import com.ritense.formflow.service.FormFlowService
+import com.ritense.valtimo.formflow.BaseIntegrationTest
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import kotlin.test.assertEquals
+
+class FormFlowStepTypeCustomComponentHandlerIT: BaseIntegrationTest() {
+
+    @Autowired
+    lateinit var formFlowService: FormFlowService
+
+    @Test
+    fun `should find correct properties for custom angular type step`() {
+        val formFlowInstance = formFlowService
+            .findDefinition("custom-component-test:latest")!!
+            .createInstance(emptyMap())
+
+        val stepProperties = formFlowService.getTypeProperties(formFlowInstance.getCurrentStep())
+
+        assertEquals(CustomComponentTypeProperties::class, stepProperties::class)
+
+        val customComponentTypeProperties = stepProperties as CustomComponentTypeProperties
+
+        assertEquals("my-component-definition", customComponentTypeProperties.id)
+    }
+}
