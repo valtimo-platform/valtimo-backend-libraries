b6be00dc14422870f575984699b7e89e54451139
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2023-11-08 13:25:33 +0100

    asyncapi test

---
+++ form-flow-valtimo/src/main/node/index.ts
@@ -0,0 +1,18 @@
+import {KotlinFileGenerator} from "@asyncapi/modelina";
+import * as path from "path";
+import {load} from "js-yaml";
+import {readFileSync} from 'fs';
+
+const generator = new KotlinFileGenerator();
+
+export async function generate() {
+    await generator.generateToFiles(
+        load(readFileSync('../resources/config/events/form-flow-events.yml', 'utf-8')) as Record<string, unknown>,
+        path.resolve(__dirname, "model"),
+        {packageName: "com.ritense.valtimo.formflow"}
+    );
+}
+
+if (require.main === module) {
+    generate();
+}
---
+++ form-flow-valtimo/src/main/node/model/AnonymousSchema_1.kt
@@ -0,0 +1,11 @@
+package com.ritense.valtimo.formflow
+
+
+data class AnonymousSchema_1(
+    val id: String? = null,
+    val instanceId: String? = null,
+    val stepKey: String? = null,
+    val order: Int? = null,
+    val submissionData: String? = null,
+    val additionalProperties: Map<String, Any>? = null,
+)
