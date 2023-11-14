35253c22eedf7b7169c33bef89d0d66d66abe1b4
Maxim Buur <maxim.buur@ritense.com>
2023-11-14 13:35:21 +0100

    pr comment

--- zgw/documenten-api/src/main/kotlin/com/ritense/documentenapi/event/DocumentInformatieObjectDownloaded.kt
+++ zgw/documenten-api/src/main/kotlin/com/ritense/documentenapi/event/DocumentInformatieObjectDownloaded.kt
@@ -4,7 +4,7 @@ import com.ritense.outbox.domain.BaseEvent
 
 class DocumentInformatieObjectDownloaded (documentInformatieobjectUrl: String) : BaseEvent(
     type = "com.ritense.gzac.drc.enkelvoudiginformatieobject.downloaded",
-    resultType = "java.io.InputStream",
+    resultType = null,
     resultId = documentInformatieobjectUrl,
     result = null
 )
