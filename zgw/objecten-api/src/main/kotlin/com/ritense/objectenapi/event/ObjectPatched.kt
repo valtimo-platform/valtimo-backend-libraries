fe7885f132942927b875069e2df8309008439395
theo-ritense <95682624+theo-ritense@users.noreply.github.com>
2023-11-16 16:08:48 +0100

    Changed resultType string from `com.ritense.objecten-api.client.` to `com.ritense.objectenapi.client.` (#1013)

--- zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/event/ObjectPatched.kt
+++ zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/event/ObjectPatched.kt
@@ -5,7 +5,7 @@ import com.ritense.outbox.domain.BaseEvent
 
 class ObjectPatched(objectUrl: String, patchedObject: ObjectNode) : BaseEvent(
     type = "com.ritense.gzac.objecten-api.object.patched",
-    resultType = "com.ritense.objecten-api.client.ObjectWrapper",
+    resultType = "com.ritense.objectenapi.client.ObjectWrapper",
     resultId = objectUrl,
     result = patchedObject
 )
--- zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/event/ObjectViewed.kt
+++ zgw/objecten-api/src/main/kotlin/com/ritense/objectenapi/event/ObjectViewed.kt
@@ -5,7 +5,7 @@ import com.ritense.outbox.domain.BaseEvent
 
 class ObjectViewed(objectUrl: String, viewedObject: ObjectNode) : BaseEvent(
     type = "com.ritense.gzac.objecten-api.object.viewed",
-    resultType = "com.ritense.objecten-api.client.ObjectWrapper",
+    resultType = "com.ritense.objectenapi.client.ObjectWrapper",
     resultId = objectUrl,
     result = viewedObject
 )
