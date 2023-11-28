29b749079aedd2e0e5e7d1f3bdfdebbb565022ec
Marijn Verbeek <marijn.verbeek@ritense.com>
2023-11-28 15:29:54 +0100

    added exporting case definition settings

--- case/src/main/kotlin/com/ritense/case/configuration/CaseAutoConfiguration.kt
+++ case/src/main/kotlin/com/ritense/case/configuration/CaseAutoConfiguration.kt
@@ -30,6 +30,7 @@ import com.ritense.case.repository.CaseTabSpecificationFactory
 import com.ritense.case.security.config.CaseHttpSecurityConfigurer
 import com.ritense.case.service.CaseDefinitionDeploymentService
 import com.ritense.case.service.CaseDefinitionService
+import com.ritense.case.service.CaseDefinitionSettingsExporter
 import com.ritense.case.service.CaseInstanceService
 import com.ritense.case.service.CaseListDeploymentService
 import com.ritense.case.service.CaseListExporter
@@ -261,5 +262,15 @@ class CaseAutoConfiguration {
     ) = CaseListExporter(
         caseDefinitionService,
         objectMapper
+    )
+
+    @Bean
+    @ConditionalOnMissingBean(CaseDefinitionSettingsExporter::class)
+    fun caseDefinitionSettingsExporter(
+        objectMapper: ObjectMapper,
+        caseDefinitionService: CaseDefinitionService,
+    ) = CaseDefinitionSettingsExporter(
+        objectMapper,
+        caseDefinitionService
     )
 }
---
+++ case/src/main/kotlin/com/ritense/case/service/CaseDefinitionSettingsExporter.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.case.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ritense.case.web.rest.dto.CaseSettingsDto
+import com.ritense.export.ExportFile
+import com.ritense.export.ExportResult
+import com.ritense.export.Exporter
+import com.ritense.export.request.DocumentDefinitionExportRequest
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+class CaseDefinitionSettingsExporter(
+    private val objectMapper: ObjectMapper,
+    private val caseDefinitionService: CaseDefinitionService
+) : Exporter<DocumentDefinitionExportRequest> {
+
+    override fun supports() = DocumentDefinitionExportRequest::class.java
+
+    override fun export(request: DocumentDefinitionExportRequest): ExportResult {
+        val caseName = request.name
+        val settings = caseDefinitionService.getCaseSettings(caseName)
+
+        val caseTabExport = ExportFile(
+            PATH.format(caseName),
+            objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsBytes(
+                    CaseSettingsDto(
+                        settings.canHaveAssignee,
+                        settings.autoAssignTasks
+                    )
+                )
+        )
+
+        return ExportResult(caseTabExport)
+    }
+
+    companion object {
+        private const val PATH = "config/case/definition/%s.json"
+    }
+}
