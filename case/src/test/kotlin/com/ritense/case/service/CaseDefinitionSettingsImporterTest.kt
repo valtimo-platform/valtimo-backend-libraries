80605daf28b7c7cfda2ce529f8e9e7e0341d80c6
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2023-12-19 13:45:59 +0100

    fix

--- case/src/test/kotlin/com/ritense/case/service/CaseDefinitionSettingsImporterTest.kt
+++ case/src/test/kotlin/com/ritense/case/service/CaseDefinitionSettingsImporterTest.kt
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.verify
 
 @ExtendWith(MockitoExtension::class)
