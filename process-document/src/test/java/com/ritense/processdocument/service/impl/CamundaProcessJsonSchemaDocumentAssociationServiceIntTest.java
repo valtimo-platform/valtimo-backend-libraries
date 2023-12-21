a1d2ad083451343cd54cc3768ac03cfa666b5ab4
Marijn Verbeek <marijn.verbeek@ritense.com>
2023-12-21 16:36:11 +0100

    fixed tests

--- process-document/src/test/java/com/ritense/processdocument/service/impl/CamundaProcessJsonSchemaDocumentAssociationServiceIntTest.java
+++ process-document/src/test/java/com/ritense/processdocument/service/impl/CamundaProcessJsonSchemaDocumentAssociationServiceIntTest.java
@@ -57,8 +57,9 @@ class CamundaProcessJsonSchemaDocumentAssociationServiceIntTest extends BaseInte
 
     @Test
     public void findProcessDocumentDefinition() {
-        final var processDocumentDefinitions = camundaProcessJsonSchemaDocumentAssociationService
-            .findProcessDocumentDefinitions(DOCUMENT_DEFINITION_NAME);
+        final var processDocumentDefinitions = AuthorizationContext
+            .runWithoutAuthorization(() -> camundaProcessJsonSchemaDocumentAssociationService
+            .findProcessDocumentDefinitions(DOCUMENT_DEFINITION_NAME));
 
         assertThat(processDocumentDefinitions.size()).isEqualTo(1);
         assertThat(processDocumentDefinitions.get(0).processDocumentDefinitionId().processDefinitionKey().toString()).isEqualTo(PROCESS_DEFINITION_KEY);
