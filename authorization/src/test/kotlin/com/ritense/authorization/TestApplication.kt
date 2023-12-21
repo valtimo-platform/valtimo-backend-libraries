9e9b2aac6b6bebfebbff72fe00ecad682852f03a
Theo <theo.beers@ritense.com>
2023-12-21 16:11:58 +0100

    Fix for securityTesting

--- authorization/src/test/kotlin/com/ritense/authorization/TestApplication.kt
+++ authorization/src/test/kotlin/com/ritense/authorization/TestApplication.kt
@@ -16,18 +16,22 @@
 
 package com.ritense.authorization
 
+import com.ritense.authorization.testimpl.TestAuthorizationAutoConfiguration
 import com.ritense.valtimo.contract.config.LiquibaseMasterChangeLogLocation
+import com.ritense.valtimo.contract.config.LiquibaseRunnerAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import javax.sql.DataSource
 
 @SpringBootApplication
+@Import(LiquibaseRunnerAutoConfiguration::class, TestAuthorizationAutoConfiguration::class, HibernateJsonMapperConfiguration::class)
 @EntityScan("com.ritense.authorization.testimpl")
 class TestApplication {
 
