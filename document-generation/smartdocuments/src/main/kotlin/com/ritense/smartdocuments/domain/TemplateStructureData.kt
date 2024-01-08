fdaea149bc84e64b070932e51dc360507851fb7b
Huib van Straten <huib.van.straten@ritense.com>
2024-01-08 11:43:08 +0100

    Feature/get templates smartdocuments (#1142)
    * -added plugin action 'get template names'
    -created the function
    
    * -WIP
    
    * - Added test for the Client
    
    * - Added test for the Plugin
    
    * - processed PR comments
    - Still need to functionally test
    
    * - changed return of plugin action. Sets now result in PV
    - For that we need the execution and a new PA-property in the action
    
    * - removed import wildcards
    
    * - removed paths from JsonProperty keys
    
    * - Dealing with XML and different responseBody structure
    
    * - Dealing with XML and different responseBody structure
    
    * - Dealing with XML and different responseBody structure
    
    * - removed paths from int test (again)
    
    * Update document-generation/smartdocuments/build.gradle
    
    Co-authored-by: Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
    
    * Update document-generation/smartdocuments/build.gradle
    
    Co-authored-by: Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
    
    * - processed pr comments
    
    * - properties public for unit testing
    
    ---------
    
    Co-authored-by: theo-ritense <95682624+theo-ritense@users.noreply.github.com>
    Co-authored-by: Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>

---
+++ document-generation/smartdocuments/src/main/kotlin/com/ritense/smartdocuments/domain/TemplateStructureData.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2015-2023 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.smartdocuments.domain
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class SmartDocumentsTemplateData(
+    @JsonProperty("DocumentsStructure")
+    val documentsStructure: DocumentsStructure
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class DocumentsStructure(
+    @JsonProperty("TemplatesStructure")
+    val templatesStructure: TemplatesStructure,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class TemplatesStructure(
+    @JsonProperty("TemplateGroups")
+    val templateGroups: List<TemplateGroup>
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class TemplateGroup(
+    @JsonProperty("Name")
+    val name: String,
+    @JsonProperty("TemplateGroups")
+    val templateGroups: List<TemplateGroup>?,
+    @JsonProperty("Templates")
+    val templates: List<Template>
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Template(
+    @JsonProperty("ID")
+    val id: String,
+    @JsonProperty("Name")
+    val name: String
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class GroupsAccess(
+    @JsonProperty("TemplateGroups")
+    val templateGroups: List<TemplateGroup>,
+)
