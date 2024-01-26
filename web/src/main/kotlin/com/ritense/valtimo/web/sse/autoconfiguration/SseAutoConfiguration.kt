cab1b34a77c208f753dda58bc6e6e1d3d9ac56eb
Ivo Zaal <ivo.zaal@ritense.com>
2024-01-26 11:18:46 +0100

    Restored SSE

--- web/src/main/kotlin/com/ritense/valtimo/web/sse/autoconfiguration/SseAutoConfiguration.kt
+++ web/src/main/kotlin/com/ritense/valtimo/web/sse/autoconfiguration/SseAutoConfiguration.kt
@@ -16,15 +16,30 @@
 
 package com.ritense.valtimo.web.sse.autoconfiguration
 
+import com.ritense.valtimo.web.sse.security.config.SseHttpSecurityConfigurer
 import com.ritense.valtimo.web.sse.service.SseSubscriptionService
+import com.ritense.valtimo.web.sse.web.rest.SseResource
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
-import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.core.annotation.Order
 
 @AutoConfiguration
 class SseAutoConfiguration {
 
     @Bean
+    @Order(270)
+    @ConditionalOnMissingBean(SseHttpSecurityConfigurer::class)
+    fun sseHttpSecurityConfigurer() = SseHttpSecurityConfigurer()
+
+    @Bean
     @ConditionalOnMissingBean(SseSubscriptionService::class)
     fun sseSubscriptionService() = SseSubscriptionService()
+
+
+    @Bean
+    @ConditionalOnMissingBean(SseResource::class)
+    fun camundaEventResource(
+        sseSubscriptionService: SseSubscriptionService
+    ) = SseResource(sseSubscriptionService)
 }
