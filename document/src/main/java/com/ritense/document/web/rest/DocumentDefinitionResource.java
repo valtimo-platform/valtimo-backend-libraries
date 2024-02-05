f2ec02afb539955fba4e624c57af76ceafc64fba
marijnritense <94897451+marijnritense@users.noreply.github.com>
2024-02-05 14:30:59 +0100

    new endpoint to retrieve template (#1189)
    * new endpoint to retrieve template
    
    * added dto for document definition templates to generate with a provided ID and name
    
    * updated endpoints
    
    * updated copyright
    
    * feedback
    
    ---------
    
    Co-authored-by: theo-ritense <95682624+theo-ritense@users.noreply.github.com>

--- document/src/main/java/com/ritense/document/web/rest/DocumentDefinitionResource.java
+++ document/src/main/java/com/ritense/document/web/rest/DocumentDefinitionResource.java
@@ -22,6 +22,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.ritense.document.domain.DocumentDefinition;
 import com.ritense.document.domain.impl.assignee.UnassignedDocumentCountDto;
+import com.ritense.document.domain.impl.template.DocumentDefinitionTemplateRequestDto;
 import com.ritense.document.service.request.DocumentDefinitionCreateRequest;
 import com.ritense.document.service.result.DeployDocumentDefinitionResult;
 import com.ritense.document.service.result.DocumentVersionsResult;
@@ -50,6 +51,9 @@ public interface DocumentDefinitionResource {
     ResponseEntity<Page<? extends DocumentDefinition>> getDocumentDefinitions(
         @PageableDefault(sort = {"id_name"}, direction = ASC) Pageable pageable
     );
+
+    @PostMapping(value = "/management/v1/document-definition-template", consumes = APPLICATION_JSON_VALUE)
+    ResponseEntity<String> getDocumentDefinitionTemplate(@Valid @RequestBody DocumentDefinitionTemplateRequestDto requestDto);
 
     @GetMapping("/management/v1/document-definition")
     ResponseEntity<Page<? extends DocumentDefinition>> getDocumentDefinitionsForManagement(
