82912af9114ad995d1b95a19dc0813218800480c
Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>
2024-03-01 13:03:02 +0100

    Added trefwoorden (#1243)

---
+++ zgw/documenten-api/src/main/kotlin/com/ritense/documentenapi/ZgwDocumentTrefwoordAutoConfiguration.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.documentenapi
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ritense.case.deployment.ZgwDocumentTrefwoordDeploymentService
+import com.ritense.documentenapi.repository.ZgwDocumentTrefwoordRepository
+import com.ritense.documentenapi.service.ZgwDocumentTrefwoordExporter
+import com.ritense.documentenapi.service.ZgwDocumentTrefwoordImporter
+import com.ritense.documentenapi.service.ZgwDocumentTrefwoordService
+import com.ritense.documentenapi.web.rest.ZgwDocumentTrefwoordResource
+import com.ritense.valtimo.changelog.service.ChangelogDeployer
+import com.ritense.valtimo.changelog.service.ChangelogService
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Bean
+
+@AutoConfiguration
+@EntityScan(basePackages = ["com.ritense.documentenapi.domain"])
+class ZgwDocumentTrefwoordAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(ZgwDocumentTrefwoordDeploymentService::class)
+    fun zgwDocumentTrefwoordDeploymentService(
+        objectMapper: ObjectMapper,
+        zgwDocumentTrefwoordRepository: ZgwDocumentTrefwoordRepository,
+        zgwDocumentTrefwoordService: ZgwDocumentTrefwoordService,
+        changelogService: ChangelogService,
+        @Value("\${valtimo.changelog.zgw-document-trefwoord.clear-tables:false}") clearTables: Boolean
+    ): ZgwDocumentTrefwoordDeploymentService {
+        return ZgwDocumentTrefwoordDeploymentService(
+            objectMapper,
+            zgwDocumentTrefwoordRepository,
+            zgwDocumentTrefwoordService,
+            changelogService,
+            clearTables
+        )
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ZgwDocumentTrefwoordService::class)
+    fun zgwDocumentTrefwoordService(
+        zgwDocumentTrefwoordRepository: ZgwDocumentTrefwoordRepository
+    ): ZgwDocumentTrefwoordService {
+        return ZgwDocumentTrefwoordService(
+            zgwDocumentTrefwoordRepository
+        )
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ZgwDocumentTrefwoordResource::class)
+    fun zgwDocumentTrefwoordResource(
+        zgwDocumentTrefwoordService: ZgwDocumentTrefwoordService
+    ): ZgwDocumentTrefwoordResource {
+        return ZgwDocumentTrefwoordResource(
+            zgwDocumentTrefwoordService
+        )
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ZgwDocumentTrefwoordExporter::class)
+    fun zgwDocumentTrefwoordExporter(
+        objectMapper: ObjectMapper,
+        zgwDocumentTrefwoordService: ZgwDocumentTrefwoordService,
+    ) = ZgwDocumentTrefwoordExporter(objectMapper, zgwDocumentTrefwoordService)
+
+    @Bean
+    @ConditionalOnMissingBean(ZgwDocumentTrefwoordImporter::class)
+    fun zgwDocumentTrefwoordImporter(
+        zgwDocumentTrefwoordDeploymentService: ZgwDocumentTrefwoordDeploymentService,
+        changelogDeployer: ChangelogDeployer,
+    ) = ZgwDocumentTrefwoordImporter(zgwDocumentTrefwoordDeploymentService, changelogDeployer)
+
+}
---
+++ zgw/documenten-api/src/test/kotlin/com/ritense/documentenapi/web/rest/ZgwDocumentTrefwoordResourceIT.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.documentenapi.web.rest
+
+import com.ritense.case.domain.CaseDefinitionSettings
+import com.ritense.case.repository.CaseDefinitionSettingsRepository
+import com.ritense.documentenapi.BaseIntegrationTest
+import com.ritense.documentenapi.service.ZgwDocumentTrefwoordService
+import com.ritense.valtimo.contract.authentication.AuthoritiesConstants.ADMIN
+import com.ritense.valtimo.contract.domain.ValtimoMediaType
+import jakarta.transaction.Transactional
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@Transactional
+internal class ZgwDocumentTrefwoordResourceIT : BaseIntegrationTest() {
+
+    @Autowired
+    lateinit var webApplicationContext: WebApplicationContext
+
+    @Autowired
+    private lateinit var service: ZgwDocumentTrefwoordService
+
+    @Autowired
+    private lateinit var caseDefinitionSettingsRepository: CaseDefinitionSettingsRepository
+
+    private lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun beforeEach() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(this.webApplicationContext)
+            .build()
+    }
+
+    @Test
+    @WithMockUser(username = "admin@ritense.com", authorities = [ADMIN])
+    fun `test getTrefwoorden`() {
+        val caseDefinitionName = "TestDefinition"
+
+        caseDefinitionSettingsRepository.save(CaseDefinitionSettings(caseDefinitionName))
+
+        service.createTrefwoord(caseDefinitionName, "Trefwoord1")
+        service.createTrefwoord(caseDefinitionName, "Trefwoord2")
+
+        mockMvc.perform(get("/api/management/v1/case-definition/{caseDefinitionName}/zgw-document/trefwoord", caseDefinitionName)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(ValtimoMediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.content").isArray)
+            .andExpect(jsonPath("$.content[0].caseDefinitionName").value("TestDefinition"))
+            .andExpect(jsonPath("$.content[0].value").value("Trefwoord1"))
+            .andExpect(jsonPath("$.content[1].caseDefinitionName").value("TestDefinition"))
+            .andExpect(jsonPath("$.content[1].value").value("Trefwoord2"))
+    }
+
+    @Test
+    @WithMockUser(username = "admin@ritense.com", authorities = [ADMIN])
+    fun `test createTrefwoord`() {
+        val caseDefinitionName = "TestDefinition"
+        val trefwoord = "TestTrefwoord"
+
+        mockMvc.perform(post("/api/management/v1/case-definition/{caseDefinitionName}/zgw-document/trefwoord/{trefwoord}", caseDefinitionName, trefwoord)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent)
+    }
+
+    @Test
+    @WithMockUser(username = "admin@ritense.com", authorities = [ADMIN])
+    fun `test deleteTrefwoord`() {
+        val caseDefinitionName = "TestDefinition"
+        val trefwoord = "TestTrefwoord"
+
+        mockMvc.perform(delete("/api/management/v1/case-definition/{caseDefinitionName}/zgw-document/trefwoord/{trefwoord}", caseDefinitionName, trefwoord)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent)
+    }
+}
