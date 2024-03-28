14d3623c9318c901ceb7a9f35ff2538bf83fb78b
Ivar Koreman <ivar.koreman@ritense.com>
2024-03-28 13:55:29 +0100

    Processed feedback.

--- process-document/src/test/java/com/ritense/processdocument/web/TaskListResourceIntTest.kt
+++ process-document/src/test/java/com/ritense/processdocument/web/TaskListResourceIntTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ritense.processdocument.web
 
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -87,7 +103,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
 
         taskListColumnRepository.saveAllAndFlush(taskListColumns)
 
-        val testUserData = startNewProcessedWithTestData()
+        val testUserData = startNewProcessesWithTestData()
 
         val taskList = taskService.createTaskQuery().active().list()
 
@@ -125,7 +141,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
     @WithMockUser(username = "user@ritense.com", authorities = [AuthoritiesConstants.USER])
     @Transactional
     fun `should get task list without caseDefinitionName`() {
-        val testUserData = startNewProcessedWithTestData()
+        val testUserData = startNewProcessesWithTestData()
 
         val taskList = taskService.createTaskQuery().active().list()
 
@@ -149,7 +165,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
             .andExpect(MockMvcResultMatchers.jsonPath("$.content[0]", hasKey("owner")))
     }
 
-    private fun startNewProcessedWithTestData(): List<Map<String, String>> {
+    private fun startNewProcessesWithTestData(): List<Map<String, String>> {
         val testUserData = listOf(
             mapOf("lastName" to "Ever", "firstName" to "Greatest"),
             mapOf("lastName" to "A.", "firstName" to "Henk"),
