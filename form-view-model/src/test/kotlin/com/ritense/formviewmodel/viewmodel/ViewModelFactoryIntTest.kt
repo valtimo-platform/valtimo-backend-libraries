1b2c2f3a5c6fdd498002995f56584c5f3a26d93c
Tom Bokma <tom.bokma@ritense.com>
2024-04-18 09:59:28 +0200

    setup new form-viewmodel module

---
+++ form-view-model/src/test/kotlin/com/ritense/formviewmodel/viewmodel/ViewModelFactoryIntTest.kt
@@ -0,0 +1,14 @@
+package com.ritense.formviewmodel.viewmodel
+
+import com.ritense.formviewmodel.BaseIntegrationTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class ViewModelFactoryIntTest : BaseIntegrationTest() {
+
+    @Test
+    fun `should create view model`() {
+        assertThat(true).isTrue()
+    }
+
+}
---
+++ form-view-model/src/test/kotlin/com/ritense/formviewmodel/viewmodel/ViewModelFactoryTest.kt
@@ -0,0 +1,14 @@
+package com.ritense.formviewmodel.viewmodel
+
+import com.ritense.formviewmodel.BaseTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class ViewModelFactoryTest : BaseTest() {
+
+    @Test
+    fun `should create view model`() {
+        assertThat(true).isTrue()
+    }
+
+}
