4819284afee844443a80827a726f889500944918
Tom Bokma <tom.bokma@ritense.com>
2024-05-16 11:01:59 +0200

    remove comment

--- form-view-model/src/test/kotlin/com/ritense/formviewmodel/event/TestSubmissionHandler.kt
+++ form-view-model/src/test/kotlin/com/ritense/formviewmodel/event/TestSubmissionHandler.kt
@@ -13,23 +13,10 @@ class TestSubmissionHandler : FormViewModelSubmissionHandler<TestViewModel> {
 
     override fun <T> handle(submission: T, task: CamundaTask) {
         submission as TestViewModel
-
-        // Documentation : a submission is a data class can be the same ViewModel
-        // Submission validate structure
-        // Can have validation on values of the submission, age must be over 18.
-        //  - Ideally the update of the VM is checking this also.
-        //  - Additional front-end validation can take place, but we want also BE checks
-        // TODO : how to handle validation errors in a generic way?
-        // Should communicate that command did not work!
-        // Command error are not use-full for a FE user this needs to go to Support.
-        // A command can have additional Business logic. How to handle this?
-        //  - A command can have exceptions
-        //  - A handler can throw runtime exceptions -> these are translated to a Generic error above the Form,
-        //      - no component relation unless you specify this in the error message mapping the component key.
-        val exampleCommand = ExampleCommand( // Can also throw FormException on runtime otherwise top level error, Developer discretion
+        val exampleCommand = ExampleCommand(
             age = submission.age!!
         )
-        dispatchCommand(exampleCommand) // Can also throw FormException on runtime otherwise top level error, Developer discretion
+        dispatchCommand(exampleCommand)
     }
 
 }
