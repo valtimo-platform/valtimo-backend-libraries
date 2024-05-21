e8220353ed731de05595a1187008aad2c15f8819
laurensvanderbilt <laurens.van.der.bilt@ritense.com>
2024-05-21 09:17:36 +0200

    added back validation

--- form-view-model/src/main/kotlin/com/ritense/formviewmodel/domain/validation/ClassPropertyExtractor.kt
+++
@@ -1,9 +0,0 @@
-package com.ritense.formviewmodel.domain.validation
-
-import com.ritense.formviewmodel.viewmodel.ViewModel
-
-class ClassPropertyExtractor {
-    fun extractProperties(clazz: Class<ViewModel>): List<String> {
-        return clazz.declaredFields.map { it.name }
-    }
-}
