f1925431f1e2c1c8e5d00aeb44e36874b958a48c
Marijn Verbeek <marijn.verbeek@ritense.com>
2024-05-23 15:08:00 +0200

    added import for case widget tabs

--- case/src/main/kotlin/com/ritense/case_/configuration/CaseWidgetAutoConfiguration.kt
+++ case/src/main/kotlin/com/ritense/case_/configuration/CaseWidgetAutoConfiguration.kt
@@ -26,6 +26,7 @@ import com.ritense.case_.rest.CaseWidgetTabManagementResource
 import com.ritense.case_.rest.CaseWidgetTabResource
 import com.ritense.case_.rest.dto.CaseWidgetTabWidgetDto
 import com.ritense.case_.service.CaseWidgetTabExporter
+import com.ritense.case_.service.CaseWidgetTabImporter
 import com.ritense.case_.service.CaseWidgetTabService
 import com.ritense.case_.widget.CaseWidgetAnnotatedClassResolver
 import com.ritense.case_.widget.CaseWidgetDataProvider
@@ -34,6 +35,7 @@ import com.ritense.case_.widget.CaseWidgetMapper
 import com.ritense.case_.widget.fields.FieldsCaseWidgetDataProvider
 import com.ritense.case_.widget.fields.FieldsCaseWidgetMapper
 import com.ritense.document.service.DocumentService
+import com.ritense.valtimo.changelog.service.ChangelogDeployer
 import com.ritense.valtimo.changelog.service.ChangelogService
 import com.ritense.valueresolver.ValueResolverService
 import org.springframework.beans.factory.annotation.Value
@@ -96,6 +98,13 @@ class CaseWidgetAutoConfiguration {
         caseTabService: CaseTabService,
         caseWidgetTabService: CaseWidgetTabService
     ) = CaseWidgetTabExporter(objectMapper, caseTabService, caseWidgetTabService)
+
+    @Bean
+    @ConditionalOnMissingBean(CaseWidgetTabImporter::class)
+    fun caseWidgetTabImporter(
+        caseWidgetTabDeployer: CaseWidgetTabDeployer,
+        changelogDeployer: ChangelogDeployer
+    ) = CaseWidgetTabImporter(caseWidgetTabDeployer, changelogDeployer)
 
     @ConditionalOnMissingBean(CaseWidgetTabResource::class)
     @Bean
