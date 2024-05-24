3fa74341960445c9c5250c06d7eb06b39020f464
Theo <theo.beers@ritense.com>
2024-05-24 11:59:43 +0200

    WIP

--- case/src/test/kotlin/com/ritense/case_/widget/table/TableCaseWidgetDataProviderTest.kt
+++ case/src/test/kotlin/com/ritense/case_/widget/table/TableCaseWidgetDataProviderTest.kt
@@ -10,6 +10,8 @@ import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
 import org.springframework.data.domain.Pageable
 import java.util.UUID
 
@@ -19,7 +21,7 @@ class TableCaseWidgetDataProviderTest(
 ) {
 
     private val caseWidgetDataProvider = TableCaseWidgetDataProvider(MapperSingleton.get(), valueResolverService)
-
+    private val objectMapper = MapperSingleton.get()
 
     @Test
     fun `should resolve data`() {
@@ -27,34 +29,56 @@ class TableCaseWidgetDataProviderTest(
         val widget = TableCaseWidget(
             "test", "Test", 0, 1, true, TableWidgetProperties(
                 collection = "test:someCollection",
-                defaultPageSize = 5,
+                defaultPageSize = 2,
                 columns = listOf(
-                    TableWidgetProperties.Column(
-                        "firstName", "Firstname", "$.firstName"
-                    ),
-                    TableWidgetProperties.Column(
-                        "lastName", "Lastname", "/lastName"
-                    ),
-                    TableWidgetProperties.Column(
-                        "partner", "Partner", "partner"
-                    )
+                    TableWidgetProperties.Column("firstName", "", "$.firstName"),
+                    TableWidgetProperties.Column("lastName", "", "/lastName"),
+                    TableWidgetProperties.Column("real", "", "real"),
+                    TableWidgetProperties.Column("age", "", "$.age"),
+                    TableWidgetProperties.Column("partnerFirstName", "", "$.partner.firstName"),
+                    TableWidgetProperties.Column("partnerLastName", "", "/partner/lastName"),
+                    TableWidgetProperties.Column("partnerReal", "", "partner/real"),
+                    TableWidgetProperties.Column("partnerAge", "", "$.partner.age"),
                 )
             )
         )
         val documentId = UUID.randomUUID()
-        whenever(valueResolverService.resolveValues(documentId.toString(), listOf(widget.properties.collection))).thenReturn(
-            mapOf(
-                widget.properties.collection to listOf(
-                    mapOf(
-                        "firstName" to "John",
-                        "lastName" to "Doe",
-//                        "partner" to "Jane Doe",
-                    )
-                )
-            )
-        )
+        val people = people()
+        mockCollection(documentId, widget, people)
 
         val data = caseWidgetDataProvider.getData(documentId, widgetTab, widget, Pageable.ofSize(widget.properties.defaultPageSize))
         assertThat(data).isNotNull
+        val jsonData = objectMapper.writeValueAsString(data)
+        JSONAssert.assertEquals("""
+
+        """.trimIndent(), jsonData, JSONCompareMode.STRICT)
     }
+
+    private fun mockCollection(documentId: UUID, widget: TableCaseWidget, collectionValue: Any?) {
+        whenever(valueResolverService.resolveValues(documentId.toString(), listOf(widget.properties.collection))).thenReturn(
+            mapOf(widget.properties.collection to collectionValue)
+        )
+    }
+
+    private fun people() = listOf(
+        john(partner = jane()),
+        john(partner = Person()),
+        Person()
+    )
+
+    private fun john(firstName: String? = "John", lastName: String? = "Doe", real: Boolean? = false, age: Int? = 30, partner: Person? = null): Person {
+        return Person(firstName, lastName, real, age, partner)
+    }
+
+    private fun jane(firstName: String? = "Jane", lastName: String? = "Doe", real: Boolean? = true, age: Int? = 25, partner: Person? = null): Person {
+        return Person(firstName, lastName, real, age, partner)
+    }
+
+    private data class Person(
+        val firstName: String? = null,
+        val lastName: String? = null,
+        val real: Boolean? = null,
+        val age: Int? = null,
+        val partner: Person? = null
+    )
 }
