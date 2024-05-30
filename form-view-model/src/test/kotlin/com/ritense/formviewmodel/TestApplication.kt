712a727101a68a28a7f1609341506bd1c49a93ff
Floris Thijssen <floris.thijssen@ritense.com>
2024-05-30 08:58:43 +0200

    process feedback

--- form-view-model/src/test/kotlin/com/ritense/formviewmodel/TestApplication.kt
+++ form-view-model/src/test/kotlin/com/ritense/formviewmodel/TestApplication.kt
@@ -17,6 +17,23 @@
 package com.ritense.formviewmodel
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.ritense.authorization.Action
+import com.ritense.authorization.AuthorizationEntityMapper
+import com.ritense.authorization.AuthorizationService
+import com.ritense.authorization.permission.Permission
+import com.ritense.authorization.request.AuthorizationRequest
+import com.ritense.authorization.role.Role
+import com.ritense.authorization.specification.AuthorizationSpecification
+import com.ritense.formviewmodel.autoconfigure.CommandDispatcherAutoConfiguration
+import com.ritense.formviewmodel.autoconfigure.FormViewModelAutoConfiguration
+import com.ritense.formviewmodel.commandhandling.Command
+import com.ritense.formviewmodel.commandhandling.CommandDispatcher
+import com.ritense.formviewmodel.commandhandling.CommandHandler
+import com.ritense.formviewmodel.commandhandling.ExampleCommandHandler
+import com.ritense.formviewmodel.event.FormViewModelSubmissionHandler
+import com.ritense.formviewmodel.event.TestSubmissionHandler
+import com.ritense.formviewmodel.viewmodel.TestViewModelLoader
+import com.ritense.valtimo.contract.config.LiquibaseRunnerAutoConfiguration
 import com.ritense.valtimo.contract.json.MapperSingleton
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
@@ -24,7 +41,9 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackageClasses = [CommandDispatcherAutoConfiguration::class, FormViewModelAutoConfiguration::class]
+)
 class TestApplication {
 
     fun main(args: Array<String>) {
@@ -39,6 +58,15 @@ class TestApplication {
         fun objectMapper(): ObjectMapper {
             return MapperSingleton.get()
         }
+
+        @Bean
+        fun testViewModelLoader() = TestViewModelLoader()
+
+        @Bean
+        fun testSubmissionHandler(): FormViewModelSubmissionHandler<*> = TestSubmissionHandler()
+
+        @Bean
+        fun exampleCommandHandler() = ExampleCommandHandler()
 
     }
 }
