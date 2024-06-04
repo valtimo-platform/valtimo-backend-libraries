95a35ffa39a19c92ff520281b06e4ae5cbff784e
laurensvanderbilt <laurens.van.der.bilt@ritense.com>
2024-06-04 16:00:32 +0200

    removed commandhandling from form-view-model

--- form-view-model/src/test/kotlin/com/ritense/formviewmodel/commandhandling/LambdaCommand.kt
+++
@@ -1,9 +0,0 @@
-package com.ritense.formviewmodel.commandhandling
-
-data class LambdaCommand(val lambda: Lambda) : Command<Unit> {
-
-    fun interface Lambda {
-        fun run()
-    }
-
-}
--- form-view-model/src/test/kotlin/com/ritense/formviewmodel/commandhandling/LambdaCommandHandler.kt
+++
@@ -1,7 +0,0 @@
-package com.ritense.formviewmodel.commandhandling
-
-class LambdaCommandHandler : CommandHandler<LambdaCommand, Unit> {
-
-    override fun execute(command: LambdaCommand) = command.lambda.run()
-
-}
