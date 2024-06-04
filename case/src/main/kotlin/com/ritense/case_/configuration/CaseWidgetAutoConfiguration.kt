986c157245bdb57affd3526880018cdcd980c80c
Maxim Buur <maxim.buur@ritense.com>
2024-06-04 13:59:15 +0200

    add custom case widget option

--- case/src/main/kotlin/com/ritense/case_/configuration/CaseWidgetAutoConfiguration.kt
+++ case/src/main/kotlin/com/ritense/case_/configuration/CaseWidgetAutoConfiguration.kt
@@ -33,6 +33,7 @@ import com.ritense.case_.widget.CaseWidgetAnnotatedClassResolver
 import com.ritense.case_.widget.CaseWidgetDataProvider
 import com.ritense.case_.widget.CaseWidgetJacksonModule
 import com.ritense.case_.widget.CaseWidgetMapper
+import com.ritense.case_.widget.custom.CustomCaseWidgetMapper
 import com.ritense.case_.widget.fields.FieldsCaseWidgetDataProvider
 import com.ritense.case_.widget.fields.FieldsCaseWidgetMapper
 import com.ritense.case_.widget.table.TableCaseWidgetDataProvider
@@ -160,4 +161,8 @@ class CaseWidgetAutoConfiguration {
         objectMapper: ObjectMapper,
         valueResolverService: ValueResolverService
     ) = TableCaseWidgetDataProvider(objectMapper, valueResolverService)
+
+    @ConditionalOnMissingBean(CustomCaseWidgetMapper::class)
+    @Bean
+    fun customCaseWidgetMapper() = CustomCaseWidgetMapper()
 }
---
+++ case/src/main/kotlin/com/ritense/case_/widget/custom/CustomCaseWidgetMapper.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.case_.widget.custom
+
+import com.ritense.case_.widget.CaseWidgetMapper
+
+class CustomCaseWidgetMapper : CaseWidgetMapper<CustomCaseWidget, CustomCaseWidgetDto> {
+    override fun supportedEntityType() = CustomCaseWidget::class.java
+
+    override fun supportedDtoType() = CustomCaseWidgetDto::class.java
+
+    override fun toDto(entity: CustomCaseWidget) = CustomCaseWidgetDto(
+        key = entity.key,
+        title = entity.title,
+        width = entity.width,
+        highContrast = entity.highContrast,
+        properties = entity.properties
+    )
+
+    override fun toEntity(dto: CustomCaseWidgetDto, index: Int) = CustomCaseWidget(
+        key = dto.key,
+        title = dto.title,
+        width = dto.width,
+        highContrast = dto.highContrast,
+        properties = dto.properties,
+        order = index
+    )
+}
