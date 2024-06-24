2ccc1ccadd42fe320eba3eafcc0f32d4944c4791
Ivar Koreman <ivar.koreman@ritense.com>
2024-06-24 16:57:45 +0200

    Added an extra test to test full task filter flow. Also fixed a bug that was discovered while writing the test.

--- process-document/src/test/java/com/ritense/processdocument/web/TaskListResourceIntTest.kt
+++ process-document/src/test/java/com/ritense/processdocument/web/TaskListResourceIntTest.kt
@@ -28,7 +28,13 @@ import com.ritense.case.repository.TaskListColumnRepository
 import com.ritense.document.domain.impl.request.NewDocumentRequest
 import com.ritense.processdocument.BaseIntegrationTest
 import com.ritense.processdocument.domain.impl.request.NewDocumentAndStartProcessRequest
+import com.ritense.processdocument.tasksearch.SearchWithConfigRequest
+import com.ritense.processdocument.tasksearch.TaskListSearchFieldV2Dto
 import com.ritense.processdocument.web.request.TaskListSearchDto
+import com.ritense.search.domain.DataType
+import com.ritense.search.domain.FieldType
+import com.ritense.search.domain.SearchFieldMatchType
+import com.ritense.search.service.SearchFieldV2Service
 import com.ritense.valtimo.contract.authentication.AuthoritiesConstants
 import com.ritense.valtimo.service.CamundaTaskService
 import java.nio.charset.StandardCharsets
@@ -37,6 +43,7 @@ import org.camunda.bpm.engine.RuntimeService
 import org.camunda.bpm.engine.TaskService
 import org.hamcrest.Matchers.hasItems
 import org.hamcrest.Matchers.hasKey
+import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -50,6 +57,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.context.WebApplicationContext
+
+private const val DOCUMENT_DEFINITION_NAME = "house"
 
 class TaskListResourceIntTest : BaseIntegrationTest() {
 
@@ -70,6 +79,9 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
     @Autowired
     lateinit var taskService: TaskService
 
+    @Autowired
+    lateinit var searchFieldV2Service: SearchFieldV2Service
+
     @BeforeEach
     fun setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
@@ -84,7 +96,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
     fun `should get task list for caseDefinitionName`() {
         val taskListColumns = listOf(
             TaskListColumn(
-                TaskListColumnId("house", "col1"),
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col1"),
                 "First name",
                 "doc:first-name",
                 DisplayType("string", EmptyDisplayTypeParameter()),
@@ -93,7 +105,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
                 0
             ),
             TaskListColumn(
-                TaskListColumnId("house", "col2"),
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col2"),
                 "Last name",
                 "doc:last-name",
                 DisplayType("string", EmptyDisplayTypeParameter()),
@@ -102,7 +114,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
                 1
             ),
             TaskListColumn(
-                TaskListColumnId("house", "col3"),
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col3"),
                 "Context",
                 "pv:context",
                 DisplayType("string", EmptyDisplayTypeParameter()),
@@ -111,7 +123,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
                 2
             ),
             TaskListColumn(
-                TaskListColumnId("house", "col4"),
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col4"),
                 "Approved",
                 "pv:approved",
                 DisplayType("boolean", EnumDisplayTypeParameter(mapOf("Yes" to "No"))),
@@ -134,7 +146,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
         mockMvc.perform(
             post("/api/v3/task")
                 .param("filter", CamundaTaskService.TaskFilter.ALL.toString())
-                .content(objectMapper.writeValueAsString(TaskListSearchDto("house")))
+                .content(objectMapper.writeValueAsString(TaskListSearchDto(DOCUMENT_DEFINITION_NAME)))
                 .characterEncoding(StandardCharsets.UTF_8)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
         )
@@ -202,6 +214,103 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
             .andExpect(jsonPath("$.content[0]", hasKey("owner")))
     }
 
+    @Test
+    @WithMockUser(username = "user@ritense.com", authorities = [AuthoritiesConstants.USER])
+    @Transactional
+    fun `should filter on something`() {
+        val firstNameToSearch = "Henk"
+
+        val taskListColumns = listOf(
+            TaskListColumn(
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col1"),
+                "First name",
+                "doc:first-name",
+                DisplayType("string", EmptyDisplayTypeParameter()),
+                false,
+                null,
+                0
+            ),
+            TaskListColumn(
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col2"),
+                "Last name",
+                "doc:last-name",
+                DisplayType("string", EmptyDisplayTypeParameter()),
+                true,
+                ColumnDefaultSort.ASC,
+                1
+            ),
+            TaskListColumn(
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col3"),
+                "Context",
+                "pv:context",
+                DisplayType("string", EmptyDisplayTypeParameter()),
+                true,
+                ColumnDefaultSort.ASC,
+                2
+            ),
+            TaskListColumn(
+                TaskListColumnId(DOCUMENT_DEFINITION_NAME, "col4"),
+                "Approved",
+                "pv:approved",
+                DisplayType("boolean", EnumDisplayTypeParameter(mapOf("Yes" to "No"))),
+                true,
+                ColumnDefaultSort.ASC,
+                3
+            )
+        )
+
+        taskListColumnRepository.saveAllAndFlush(taskListColumns)
+
+        searchFieldV2Service.create(TaskListSearchFieldV2Dto(
+            ownerId = DOCUMENT_DEFINITION_NAME,
+            key = "firstName",
+            title = "First name",
+            path = "doc:first-name",
+            order = 0,
+            dataType = DataType.TEXT,
+            matchType = SearchFieldMatchType.LIKE,
+            fieldType = FieldType.SINGLE
+        ))
+
+        val testUserData = startNewProcessesWithTestData()
+
+        val taskList = taskService.createTaskQuery().active().list()
+
+        assertThat(taskList).hasSize(testUserData.size)
+
+        val expectedUserData = testUserData.find { it["firstName"] == firstNameToSearch }
+
+        val filter = SearchWithConfigRequest.SearchWithConfigFilter()
+        filter.key = "firstName"
+        filter.setValues(listOf(firstNameToSearch))
+
+        val searchWithConfigRequest = SearchWithConfigRequest()
+        searchWithConfigRequest.otherFilters = listOf(filter)
+
+        mockMvc.perform(
+            post("/api/v1/document-definition/{caseDefinitionName}/task/search", DOCUMENT_DEFINITION_NAME)
+                .content(objectMapper.writeValueAsString(searchWithConfigRequest))
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.content").isArray)
+            .andExpect(jsonPath("$.content", hasSize<Any>(1)))
+            .andExpect(jsonPath("$.content[0].items").isArray)
+            .andExpect(jsonPath("$.content[0].items[0].key").value("col1"))
+            .andExpect(jsonPath("$.content[0].items[0].value").value(expectedUserData!!["firstName"]))
+            .andExpect(jsonPath("$.content[0].items[1].key").value("col2"))
+            .andExpect(jsonPath("$.content[0].items[1].value").value(expectedUserData["lastName"]))
+            .andExpect(jsonPath("$.content[0].items[2].key").value("col3"))
+            .andExpect(jsonPath("$.content[0].items[2].value").value(expectedUserData["context"]))
+            .andExpect(jsonPath("$.content[0].items[3].key").value("col4"))
+            .andExpect(jsonPath("$.content[0].items[3].value").value(expectedUserData["approved"]))
+            .andExpect(jsonPath("$.totalElements").value(1))
+            .andExpect(jsonPath("$.sort[0].property").value("doc:last-name"))
+            .andExpect(jsonPath("$.sort[0].direction").value("ASC"))
+    }
+
     private fun startNewProcessesWithTestData(): List<Map<String, Any?>> {
         val testUserData = listOf(
             mapOf("lastName" to "Ever", "firstName" to "Greatest", "context" to "Amsterdam", "approved" to true),
@@ -217,7 +326,7 @@ class TaskListResourceIntTest : BaseIntegrationTest() {
             val startRequest = NewDocumentAndStartProcessRequest(
                 "single-user-task-process",
                 NewDocumentRequest(
-                    "house",
+                    DOCUMENT_DEFINITION_NAME,
                     objectMapper.readTree("""{ "first-name": $firstName, "last-name": $lastName}""")
                 )
             ).withProcessVars(mapOf("context" to context, "approved" to approved))
