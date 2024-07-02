7bec06f0dc97adadcec91c518f53bae171226af4
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2024-07-02 11:39:21 +0200

    #113116 #113117 Import & Export task search fields

---
+++ search/src/main/kotlin/com/ritense/search/exporter/SearchFieldExporter.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.search.exporter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ritense.exporter.ExportFile
+import com.ritense.exporter.ExportPrettyPrinter
+import com.ritense.exporter.ExportResult
+import com.ritense.exporter.Exporter
+import com.ritense.exporter.request.DocumentDefinitionExportRequest
+import com.ritense.search.service.SearchFieldV2Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
+
+@Transactional(readOnly = true)
+abstract class SearchFieldExporter(
+    private val objectMapper: ObjectMapper,
+    private val searchFieldService: SearchFieldV2Service
+) : Exporter<DocumentDefinitionExportRequest> {
+
+    override fun supports() = DocumentDefinitionExportRequest::class.java
+
+    override fun export(request: DocumentDefinitionExportRequest): ExportResult {
+        val documentDefinitionName = request.name
+        val searchFields = searchFieldService.findAllByOwnerTypeAndOwnerId(ownerTypeKey(), documentDefinitionName)
+
+        if (searchFields.isEmpty()) {
+            return ExportResult()
+        }
+
+        val caseTaskListChangeset = SearchFieldExportChangeset(
+            "$documentDefinitionName.${ownerTypeKey()}.${Instant.now().toEpochMilli()}",
+            listOf(
+                SearchFieldExportCollection(
+                    documentDefinitionName,
+                    searchFields.map { ExportFileSearchFieldDto.from(it) }
+                )
+            )
+        )
+        val caseTaskListExport = ExportFile(
+            getPath(documentDefinitionName),
+            objectMapper.writer(ExportPrettyPrinter()).writeValueAsBytes(caseTaskListChangeset)
+        )
+
+        return ExportResult(
+            caseTaskListExport
+        )
+    }
+
+    abstract fun ownerTypeKey(): String
+
+    abstract fun getPath(documentDefinitionName: String): String
+}
