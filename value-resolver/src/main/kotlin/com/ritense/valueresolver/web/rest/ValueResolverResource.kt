9750184d31d48f36a62b91ef663374f43bb0daa5
Maxim Buur <maxim.buur@ritense.com>
2024-07-15 10:35:52 +0200

    fix bootrun

--- value-resolver/src/main/kotlin/com/ritense/valueresolver/web/rest/ValueResolverResource.kt
+++ value-resolver/src/main/kotlin/com/ritense/valueresolver/web/rest/ValueResolverResource.kt
@@ -1,12 +1,16 @@
 package com.ritense.valueresolver.web.rest
 
+import com.ritense.valtimo.contract.annotation.SkipComponentScan
+import com.ritense.valtimo.contract.domain.ValtimoMediaType.APPLICATION_JSON_UTF8_VALUE
 import com.ritense.valueresolver.ValueResolverService
-import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@SkipComponentScan
 @RequestMapping("/api", produces = [APPLICATION_JSON_UTF8_VALUE])
 class ValueResolverResource(
     private val valueResolverService: ValueResolverService
