376cd015d5d60ef72b6b97d41e55744b20d5340b
marijnritense <94897451+marijnritense@users.noreply.github.com>
2024-07-16 10:28:59 +0200

    Added user identifier toggle (#1471)
    ---------
    
    Co-authored-by: Theo <theo.beers@ritense.com>

--- contract/src/main/java/com/ritense/valtimo/contract/authentication/ManageableUser.java
+++ contract/src/main/java/com/ritense/valtimo/contract/authentication/ManageableUser.java
@@ -16,6 +16,8 @@
 
 package com.ritense.valtimo.contract.authentication;
 
+import com.ritense.valtimo.contract.OauthConfigHolder;
+
 public interface ManageableUser extends AuthorizedUser {
 
     String getId();
@@ -45,5 +47,12 @@ public interface ManageableUser extends AuthorizedUser {
     void deactivate();
 
     boolean isBlocked();
+
+    default String getUserIdentifier() {
+        return switch (OauthConfigHolder.getCurrentInstance().getIdentifierField()) {
+            case USERID -> getId();
+            case USERNAME -> getUsername();
+        };
+    }
 
 }
