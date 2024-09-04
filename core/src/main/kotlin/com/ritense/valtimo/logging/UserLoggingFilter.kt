79715df0c778d90429fb9907201705f674239223
Ivo Zaal <74657121+ivo-ritense@users.noreply.github.com>
2024-09-04 09:43:31 +0200

    Added logging context annotation (#1531)

---
+++ core/src/main/kotlin/com/ritense/valtimo/logging/UserLoggingFilter.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.valtimo.logging
+
+import com.ritense.valtimo.contract.LoggingConstants
+import com.ritense.valtimo.contract.authentication.UserManagementService
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.MDC
+import org.springframework.web.filter.OncePerRequestFilter
+import java.util.UUID
+
+class UserLoggingFilter(
+    private val userManagementService: UserManagementService
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val userIdentifier = userManagementService.getCurrentUser()?.getUserIdentifier()
+        if (userIdentifier != null) {
+            MDC.put(LoggingConstants.MDC_USER_ID_KEY, userIdentifier)
+        }
+
+        // We also add a correlation id manually for now.
+        MDC.put(LoggingConstants.MDC_CORRELATION_ID_KEY, UUID.randomUUID().toString())
+
+        try {
+            filterChain.doFilter(request, response)
+        } finally {
+            MDC.remove(LoggingConstants.MDC_USER_ID_KEY)
+            MDC.remove(LoggingConstants.MDC_CORRELATION_ID_KEY)
+        }
+    }
+}
---
+++ logging/src/test/kotlin/com/ritense/logging/testimpl/LogResource.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.logging.testimpl
+
+import mu.KLogger
+import mu.KotlinLogging
+import mu.withLoggingContext
+
+class LogResource() {
+
+    val id = 123
+
+    fun logSomething() {
+        withLoggingContext(this::class.java.canonicalName to "some-key") {
+            outputMessage()
+        }
+    }
+
+    fun logNothing() {
+        withLoggingContext(OtherInnerClass::class.java.canonicalName to "should-not-be-logged") {
+            //we don't want to log anything here to validate the key is removed from the MDC
+        }
+    }
+
+    fun logSomethingWithTwoLevelsOfMetaData() {
+        withLoggingContext(InnerClass::class.java.canonicalName to "second-key") {
+            logSomething()
+        }
+    }
+
+    fun logSomethingWithSiblingResource() {
+        logNothing()
+        logSomething()
+    }
+
+    fun outputMessage() {
+        logger.info { "This is a message" }
+    }
+
+    class InnerClass
+    class OtherInnerClass
+
+    private companion object {
+        val logger: KLogger = KotlinLogging.logger {}
+    }
+}
