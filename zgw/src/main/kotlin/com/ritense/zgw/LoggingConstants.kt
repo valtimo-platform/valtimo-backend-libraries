51b1f56e6d0a625c0f56a49d452bcd196391d21d
Maxim Buur <maxim.buur@ritense.com>
2024-09-10 13:25:17 +0200

    catalogi api logging

--- zgw/src/main/kotlin/com/ritense/zgw/LoggingConstants.kt
+++ zgw/src/main/kotlin/com/ritense/zgw/LoggingConstants.kt
@@ -12,4 +12,14 @@ object LoggingConstants {
         const val BESLUIT = "Besluiten API: besluit"
     }
 
+    object CATALOGI_API {
+        const val STATUSTYPE = "Catalogi API: statustype"
+        const val RESULTAATTYPE = "Catalogi API: resultaattype"
+        const val BESLUITTYPE = "Catalogi API: besluittype"
+        const val ZAAKTYPEINFORMATIEOBJECTTYPE = "Catalogi API: zaaktype-informatieobjecttype"
+        const val INFORMATIEOBJECTTYPE = "Catalogi API: informatieobjecttype"
+        const val ROLTYPE = "Catalogi API: roltype"
+        const val EIGENSCHAP = "Catalogi API: eigenschap"
+        const val ZAAKTYPE = "Catalogi API: zaaktype"
+    }
 }
