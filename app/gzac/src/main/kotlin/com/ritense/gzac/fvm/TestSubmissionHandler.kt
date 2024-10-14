49745b27bce31842697d39c06307d85a5af2bddd
Tom Bokma <tom.bokma@ritense.com>
2024-10-14 13:38:23 +0200

    added example

--- app/gzac/src/main/kotlin/com/ritense/gzac/fvm/TestSubmissionHandler.kt
+++ app/gzac/src/main/kotlin/com/ritense/gzac/fvm/TestSubmissionHandler.kt
@@ -2,6 +2,7 @@ package com.ritense.gzac.fvm
 
 import com.ritense.commandhandling.dispatchCommand
 import com.ritense.formviewmodel.commandhandling.CompleteTaskCommand
+import com.ritense.formviewmodel.error.FormException
 import com.ritense.formviewmodel.submission.FormViewModelUserTaskSubmissionHandler
 import com.ritense.valtimo.camunda.domain.CamundaTask
 import org.springframework.stereotype.Component
@@ -11,6 +12,7 @@ class TestSubmissionHandler : FormViewModelUserTaskSubmissionHandler<TestViewMod
     override fun supports(formName: String): Boolean = formName == "fvm-test"
 
     override fun <T> handle(submission: T, task: CamundaTask, businessKey: String) {
+        throw FormException("This is a Business error", "test")
         dispatchCommand(
             CompleteTaskCommand(
                 taskId = task.id
