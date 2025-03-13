55a1598da2544b0543ed1a0f23045b030386920a
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2025-03-13 14:01:14 +0100

    fix case deletion

--- core/src/main/java/com/ritense/valtimo/service/CamundaProcessService.java
+++ core/src/main/java/com/ritense/valtimo/service/CamundaProcessService.java
@@ -160,7 +160,7 @@ public class CamundaProcessService {
 
     public void deleteProcessInstanceById(String processInstanceId, String reason) {
         denyAuthorization();
-        runtimeService.deleteProcessInstance(processInstanceId, reason);
+        runtimeService.deleteProcessInstance(processInstanceId, reason, true, true, true, false);
     }
 
     public void removeProcessVariables(String processInstanceId, Collection<String> variableNames) {
