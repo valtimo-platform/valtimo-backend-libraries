68f2a1cd0b68a10b541b830041c1b5057f4eeec9
erik-ritense <83765770+erik-ritense@users.noreply.github.com>
2025-04-02 14:45:56 +0200

    Story/case tags 132892 (#1750)
    * gzac-issue 93: added case tag functionality
    
    ---------
    
    Co-authored-by: valtimo-platform[bot] <80107705+valtimo-platform[bot]@users.noreply.github.com>
    Co-authored-by: Maxim Buur <maxim.buur@ritense.com>

---
+++ document/src/main/kotlin/com/ritense/document/repository/CaseTagRepository.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.document.repository
+
+import com.ritense.document.domain.CaseTag
+import com.ritense.document.domain.CaseTagId
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+
+interface CaseTagRepository : JpaRepository<CaseTag, CaseTagId> {
+    fun findByIdCaseDefinitionNameOrderByOrder(caseDefinitionName: String): List<CaseTag>
+    fun findDistinctByIdCaseDefinitionNameAndIdKey(caseDefinitionName: String, key: String): CaseTag?
+    fun existsByIdCaseDefinitionNameAndIdKey(caseDefinitionName: String, key: String): Boolean
+    @Query(
+        value = """
+        SELECT EXISTS (
+            SELECT 1 FROM case_tag_link
+            WHERE case_tag_key = :caseTagKey
+            AND case_definition_name = :caseDefinitionName
+        )
+        """, nativeQuery = true
+    )
+    fun isCaseTagInUse(
+        @Param("caseTagKey") caseTagKey: String,
+        @Param("caseDefinitionName") caseDefinitionName: String
+    ): Boolean
+}
