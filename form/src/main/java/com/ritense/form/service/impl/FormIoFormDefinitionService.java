7375a314bdcef330bbad74bf1fd812a9587fbc19
Maxim Buur <maxim.buur@ritense.com>
2025-04-03 10:56:56 +0200

    only form definitions without case id for general endpoint

--- form/src/main/java/com/ritense/form/service/impl/FormIoFormDefinitionService.java
+++ form/src/main/java/com/ritense/form/service/impl/FormIoFormDefinitionService.java
@@ -43,7 +43,7 @@ public class FormIoFormDefinitionService implements FormDefinitionService {
 
     @Override
     public Page<FormIoFormDefinition> getAll(Pageable pageable) {
-        return formDefinitionRepository.findAll(pageable);
+        return formDefinitionRepository.findByCaseDefinitionIdIsNull(pageable);
     }
 
     @Override
@@ -56,7 +56,7 @@ public class FormIoFormDefinitionService implements FormDefinitionService {
 
     @Override
     public Page<? extends FormDefinition> queryFormDefinitions(String searchTerm, Pageable pageable) {
-        return formDefinitionRepository.findAllByNameContainingIgnoreCase(searchTerm, pageable);
+        return formDefinitionRepository.findAllWithoutCaseByNameContainingIgnoreCase(searchTerm, pageable);
     }
 
     @Override
