7e660b5c61cf6d4bb5b4777b3e76473cd8def50b
klaasschuijtemaker <klaas.schuijtemaker@ritense.com>
2025-04-08 14:42:47 +0200

    Submission test - Array in formio

--- form/src/test/kotlin/com/ritense/form/service/DefaultFormSubmissionServiceIntTest.kt
+++ form/src/test/kotlin/com/ritense/form/service/DefaultFormSubmissionServiceIntTest.kt
@@ -92,12 +92,21 @@ class DefaultFormSubmissionServiceIntTest @Autowired constructor(
         assertThat(json, hasNoJsonPath("""${'$'}.hiddenInputFalse"""))
         assertThat(json, hasNoJsonPath("""${'$'}.inputDisabled"""))
         assertThat(json, hasJsonPath("""${'$'}.arrayInDocument[0].property1""", equalTo("property1")))
-        assertThat(json, hasJsonPath("""${'$'}.arrayInDocument[1].property2""", equalTo("property2")))
+        assertThat(json, hasJsonPath("""${'$'}.arrayInDocument[0].property2""", equalTo("property2")))
+        assertThat(json, hasNoJsonPath("""${'$'}.arrayInDocument[0].property3"""))
         assertThat(json, hasNoJsonPath("""${'$'}.property1"""))
         assertThat(json, hasNoJsonPath("""${'$'}.property2"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.property3"""))
         assertThat(json, hasJsonPath("""${'$'}.informatieverzoeken[0].verzoek.jaartallen""", equalTo("2010")))
         assertThat(json, hasJsonPath("""${'$'}.informatieverzoeken[0].verzoek.toelichting""", equalTo("From 2010")))
+        assertThat(json, hasNoJsonPath("""${'$'}.informatieverzoeken[0].verzoek.missing"""))
         assertThat(json, hasNoJsonPath("""${'$'}.verzoek"""))
+        assertThat(json, hasJsonPath("""${'$'}.containerArray[0].container.containerProperty1""", equalTo("containerProperty1")))
+        assertThat(json, hasJsonPath("""${'$'}.containerArray[0].container.containerProperty2""", equalTo("containerProperty2")))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerArray[0].container.containerProperty3"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerProperty1"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerProperty2"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerProperty3"""))
 
         val processExecution = runWithoutAuthorization {
             processService.findExecutionByBusinessKey(businessKey)
@@ -171,12 +180,18 @@ class DefaultFormSubmissionServiceIntTest @Autowired constructor(
         assertThat(json, hasNoJsonPath("""${'$'}.hiddenInputFalse"""))
         assertThat(json, hasNoJsonPath("""${'$'}.inputDisabled"""))
         assertThat(json, hasJsonPath("""${'$'}.arrayInDocument[0].property1""", equalTo("property1")))
-        assertThat(json, hasJsonPath("""${'$'}.arrayInDocument[1].property2""", equalTo("property2")))
+        assertThat(json, hasJsonPath("""${'$'}.arrayInDocument[0].property2""", equalTo("property2")))
         assertThat(json, hasNoJsonPath("""${'$'}.property1"""))
         assertThat(json, hasNoJsonPath("""${'$'}.property2"""))
         assertThat(json, hasJsonPath("""${'$'}.informatieverzoeken[0].verzoek.jaartallen""", equalTo("2010")))
         assertThat(json, hasJsonPath("""${'$'}.informatieverzoeken[0].verzoek.toelichting""", equalTo("From 2010")))
         assertThat(json, hasNoJsonPath("""${'$'}.verzoek"""))
+        assertThat(json, hasJsonPath("""${'$'}.containerArray[0].container.containerProperty1""", equalTo("containerProperty1")))
+        assertThat(json, hasJsonPath("""${'$'}.containerArray[0].container.containerProperty2""", equalTo("containerProperty2")))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerArray[0].container.containerProperty3"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerProperty1"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerProperty2"""))
+        assertThat(json, hasNoJsonPath("""${'$'}.containerProperty3"""))
     }
 
     private fun createFormData(): JsonNode {
@@ -194,7 +209,8 @@ class DefaultFormSubmissionServiceIntTest @Autowired constructor(
                 "inputDisabled": "test-value",
                 "property1": "property1",
                 "property2": "property2",
-                "verzoek": {"jaartallen":"2010","toelichting":"From 2010"}
+                "verzoek": {"jaartallen":"2010","toelichting":"From 2010"},
+                "container": {"containerProperty1":"containerProperty1","containerProperty2":"containerProperty2"}
             }
         """.trimIndent()
         )
