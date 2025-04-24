19efb37e6ba5582271236a621c2d09a1e50fe727
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2025-04-24 15:08:16 +0200

    #136978 Duplicate DocumentObjectenApiSync (#1809)
    Co-authored-by: valtimo-platform[bot] <80107705+valtimo-platform[bot]@users.noreply.github.com>

--- zgw/zaakdetails/src/main/kotlin/com/ritense/zaakdetails/ZaakDetailsAutoConfiguration.kt
+++ zgw/zaakdetails/src/main/kotlin/com/ritense/zaakdetails/ZaakDetailsAutoConfiguration.kt
@@ -21,6 +21,7 @@ import com.ritense.objectenapi.management.ObjectManagementInfoProvider
 import com.ritense.plugin.service.PluginService
 import com.ritense.valtimo.contract.case_.CaseDefinitionChecker
 import com.ritense.valtimo.contract.config.LiquibaseMasterChangeLogLocation
+import com.ritense.zaakdetails.documentobjectenapisync.DocumentObjectenApiSyncCaseEventListener
 import com.ritense.zaakdetails.documentobjectenapisync.DocumentObjectenApiSyncManagementResource
 import com.ritense.zaakdetails.documentobjectenapisync.DocumentObjectenApiSyncManagementService
 import com.ritense.zaakdetails.documentobjectenapisync.DocumentObjectenApiSyncRepository
@@ -112,5 +113,15 @@ class ZaakDetailsAutoConfiguration {
         zaakdetailsObjectRepository: ZaakdetailsObjectRepository
     ): ZaakdetailsObjectService {
         return ZaakdetailsObjectService(zaakdetailsObjectRepository)
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(DocumentObjectenApiSyncCaseEventListener::class)
+    fun documentObjectenApiSyncCaseEventListener(
+        documentObjectenApiSyncManagementService: DocumentObjectenApiSyncManagementService,
+    ): DocumentObjectenApiSyncCaseEventListener {
+        return DocumentObjectenApiSyncCaseEventListener(
+            documentObjectenApiSyncManagementService,
+        )
     }
 }
