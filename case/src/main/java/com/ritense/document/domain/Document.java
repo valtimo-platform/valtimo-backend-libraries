85257e025962bf6902b6a3337a697054f4d9559b
Ivo Zaal <ivo.zaal@ritense.com>
2025-05-02 15:07:30 +0200

    Based document content filter on application properties

--- case/src/main/java/com/ritense/document/domain/Document.java
+++ case/src/main/java/com/ritense/document/domain/Document.java
@@ -16,9 +16,12 @@
 
 package com.ritense.document.domain;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.ritense.document.domain.impl.DocumentContentFilter;
 import com.ritense.document.domain.relation.DocumentRelation;
 import com.ritense.document.web.rest.dto.CaseTagResponseDto;
 import java.time.LocalDateTime;
@@ -27,6 +30,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+@JsonFilter("documentContentFilter")
 public interface Document {
 
     @JsonProperty
@@ -56,7 +60,7 @@ public interface Document {
     @JsonProperty
     String assigneeFullName();
 
-    @JsonProperty
+    @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = DocumentContentFilter.class)
     DocumentContent content();
 
     @JsonProperty
