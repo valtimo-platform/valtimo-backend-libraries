d218bb55a21bac0cb5e1abfd2de79179c2eca71e
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2025-05-07 16:46:04 +0200

    #129637 remove connector feature (#1815)

--- mail/flowmailer/src/main/kotlin/com/ritense/mail/flowmailer/autoconfigure/FlowmailerAutoConfiguration.kt
+++ mail/flowmailer/src/main/kotlin/com/ritense/mail/flowmailer/autoconfigure/FlowmailerAutoConfiguration.kt
@@ -17,22 +17,15 @@
 package com.ritense.mail.flowmailer.autoconfigure
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.ritense.connector.domain.Connector
-import com.ritense.document.service.DocumentService
 import com.ritense.mail.MailDispatcher
 import com.ritense.mail.flowmailer.config.FlowmailerProperties
-import com.ritense.mail.flowmailer.connector.FlowmailerConnector
-import com.ritense.mail.flowmailer.connector.FlowmailerConnectorProperties
 import com.ritense.mail.flowmailer.service.FlowmailerMailDispatcher
 import com.ritense.mail.flowmailer.service.FlowmailerTokenService
-import com.ritense.resource.service.ResourceService
-import org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Scope
 import org.springframework.web.client.RestTemplate
 
 @AutoConfiguration
@@ -63,32 +56,5 @@ class FlowmailerAutoConfiguration {
         restTemplate: RestTemplate
     ): FlowmailerTokenService {
         return FlowmailerTokenService(flowmailerProperties, restTemplate)
-    }
-
-    //Connector
-
-    @Bean
-    @ConditionalOnMissingBean(FlowmailerConnector::class)
-    @Scope(SCOPE_PROTOTYPE)
-    fun flowmailerConnector(
-        flowmailerConnectorProperties: FlowmailerConnectorProperties,
-        mailDispatcher: MailDispatcher,
-        documentService: DocumentService,
-        resourceService: ResourceService
-    ): Connector {
-        return FlowmailerConnector(
-            flowmailerConnectorProperties,
-            mailDispatcher,
-            documentService,
-            resourceService
-        )
-    }
-
-    @Bean
-    @Scope(SCOPE_PROTOTYPE)
-    fun flowmailerConnectorProperties(
-        flowmailerProperties: FlowmailerProperties
-    ): FlowmailerConnectorProperties {
-        return FlowmailerConnectorProperties(flowmailerProperties)
     }
 }
