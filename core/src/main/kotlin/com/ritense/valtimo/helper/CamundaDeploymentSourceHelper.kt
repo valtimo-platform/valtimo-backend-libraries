e6337fef6b55de85d8eed54de4d3b9359d357616
Maxim Buur <maxim.buur@ritense.com>
2025-06-03 10:07:45 +0200

    fix tests

--- core/src/main/kotlin/com/ritense/valtimo/helper/CamundaDeploymentSourceHelper.kt
+++ core/src/main/kotlin/com/ritense/valtimo/helper/CamundaDeploymentSourceHelper.kt
@@ -1,5 +1,20 @@
-package com.ritense.valtimo.helper
+/*
+ *  Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ *  Licensed under EUPL, Version 1.2 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
+package com.ritense.valtimo.helper
 
 import com.ritense.valtimo.camunda.domain.CamundaDeploymentSource
 import java.util.UUID
@@ -14,7 +29,8 @@ class CamundaDeploymentSourceHelper {
         return uuid
     }
 
-    fun retrieve(uuid: String): CamundaDeploymentSource? {
-        return store.remove(uuid)
+    fun retrieve(uuid: String?): CamundaDeploymentSource {
+        if (uuid == null) return CamundaDeploymentSource()
+        return store.remove(uuid) ?: CamundaDeploymentSource()
     }
 }
