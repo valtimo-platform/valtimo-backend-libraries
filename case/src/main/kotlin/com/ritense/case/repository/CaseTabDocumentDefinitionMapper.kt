0b575c731f4d87e97532d092f849305859fc5ee9
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2025-06-18 07:25:47 +0200

    #106335 Find and implement an alternative to grouping on id in PBAC

--- case/src/main/kotlin/com/ritense/case/repository/CaseTabDocumentDefinitionMapper.kt
+++ case/src/main/kotlin/com/ritense/case/repository/CaseTabDocumentDefinitionMapper.kt
@@ -48,14 +48,22 @@ class CaseTabDocumentDefinitionMapper(
         query: AbstractQuery<*>,
         criteriaBuilder: CriteriaBuilder
     ): AuthorizationEntityMapperResult<JsonSchemaDocumentDefinition> {
-        val documentDefinitionRoot: Root<JsonSchemaDocumentDefinition> = query.from(JsonSchemaDocumentDefinition::class.java)
-        return AuthorizationEntityMapperResult(
-            documentDefinitionRoot,
-            query,
-            criteriaBuilder.equal(
-                root.get<CaseTabId>("id").get<String>("caseDefinitionName"),
-                documentDefinitionRoot.get<JsonSchemaDocumentDefinitionId>("id").get<String>("name")
+
+        val subquery = query.subquery(Int::class.java)
+        val subRoot = subquery.from(JsonSchemaDocumentDefinition::class.java)
+
+        subquery.select(criteriaBuilder.literal(1))
+            .where(
+                criteriaBuilder.equal(
+                    root.get<CaseTabId>("id").get<String>("caseDefinitionName"),
+                    subRoot.get<JsonSchemaDocumentDefinitionId>("id").get<String>("name")
+                )
             )
+
+        return AuthorizationEntityMapperResult(
+            subRoot,
+            subquery,
+            criteriaBuilder.exists(subquery)
         )
     }
 
