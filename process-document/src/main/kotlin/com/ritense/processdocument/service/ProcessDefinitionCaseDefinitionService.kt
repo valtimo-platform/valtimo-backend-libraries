7d97c348a0614c3355e162eb71f462c65771d5d9
Ivo Zaal <ivo.zaal@ritense.com>
2025-07-01 11:14:34 +0200

    Operaton fix

--- process-document/src/main/kotlin/com/ritense/processdocument/service/ProcessDefinitionCaseDefinitionService.kt
+++ process-document/src/main/kotlin/com/ritense/processdocument/service/ProcessDefinitionCaseDefinitionService.kt
@@ -30,12 +30,12 @@ import com.ritense.processdocument.domain.ProcessDocumentDefinitionRequest
 import com.ritense.processdocument.domain.UpdateProcessDefinitionCaseDefinitionRequest
 import com.ritense.processdocument.domain.impl.OperatonProcessInstanceId
 import com.ritense.processdocument.repository.ProcessDefinitionCaseDefinitionRepository
+import com.ritense.valtimo.contract.case_.CaseDefinitionChecker
+import com.ritense.valtimo.contract.case_.CaseDefinitionId
 import com.ritense.valtimo.operaton.authorization.OperatonExecutionActionProvider
 import com.ritense.valtimo.operaton.domain.OperatonExecution
 import com.ritense.valtimo.operaton.domain.OperatonProcessDefinition
 import com.ritense.valtimo.operaton.service.OperatonRepositoryService
-import com.ritense.valtimo.contract.case_.CaseDefinitionChecker
-import com.ritense.valtimo.contract.case_.CaseDefinitionId
 import org.operaton.bpm.engine.RuntimeService
 import java.util.UUID
 
@@ -75,9 +75,10 @@ class ProcessDefinitionCaseDefinitionService(
                     document.definitionId().caseDefinitionId()
                 )
             )
-            processDefinitionCaseDefinition.processDefinitionName =
-                repositoryService.findProcessDefinitionById(processInstance.processDefinitionId)!!.name
-            processDefinitionCaseDefinition.processDefinitionKey = processInstance.processDefinitionKey
+
+            val processDefinition = repositoryService.findProcessDefinitionById(processInstance.processDefinitionId)!!
+            processDefinitionCaseDefinition.processDefinitionName = processDefinition.name
+            processDefinitionCaseDefinition.processDefinitionKey = processDefinition.key
 
             return processDefinitionCaseDefinition
         }
