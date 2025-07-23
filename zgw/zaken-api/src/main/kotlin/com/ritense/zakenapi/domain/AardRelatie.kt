2bae217f152509f0dc0c656c090477add43098af
Maarten van Toor <maarten.van.toor@ritense.com>
2025-07-23 17:07:49 +0200

    added overig type

--- zgw/zaken-api/src/main/kotlin/com/ritense/zakenapi/domain/AardRelatie.kt
+++ zgw/zaken-api/src/main/kotlin/com/ritense/zakenapi/domain/AardRelatie.kt
@@ -5,5 +5,6 @@ import com.fasterxml.jackson.annotation.JsonValue
 enum class AardRelatie(@JsonValue val key: String) {
     BIJDRAGE("bijdrage"),
     ONDERWERP("onderwerp"),
+    OVERIG("overig"),
     VERVOLG("vervolg"),
 }
